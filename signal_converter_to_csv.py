739f4530524083970541e507824c9a9dd7dbb66e
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-02-14 23:44:45 +1100

    adds a CLI input in the form of agilent_signal_converter, and adds a filter for .DS_Store files, which were being caught by the .D if condition

--- signal_converter_to_csv.py
+++ signal_converter_to_csv.py
@@ -8,12 +8,24 @@ import rainbow as rb
 
 import re
 
+# input a string filepath of the directory containing all .D files.
 
-def signal_converter_to_csv(path: str):
+def signal_converter_to_csv(path: str, dry_run = True):
+
+    # converts the string filepath to a path object.
 
     p = Path(path)
 
-    data_dir_file_path_list = [x for x in p.iterdir() if ".D" in x.name]
+    # creates a list of path objects if their file extension is .D, these are the target dirs.
+
+    data_dir_file_path_list = [x for x in p.iterdir() if ".D" in x.name and ".DS_Store" not in x.name]
+
+    """
+    This loop is the core of this module. For each .D path object it creates rainbow objects, creates a
+    csv_files dir in the .D dir if it doesnt exist, assembles csv file path objects from the name of each
+    .ch file, and then calls the rainbow.export_csv() method on each rainbow object in the list. Also contains
+    an option for a dry run.
+    """
 
     for data_dir_file_path in data_dir_file_path_list:
         print("datadir file path:", data_dir_file_path)
@@ -41,6 +53,8 @@ def signal_converter_to_csv(path: str):
                 for datafile in datadir.datafiles:
                     csv_data_file_name = str(datafile).replace(".ch", ".csv")
 
+                    # in order to retain fractional wavelength measurements in the file name, the decimal place is replaced with an underscore
+
                     csv_data_file_name = re.sub("DAD1.", "{}".format(datafile.ylabels[0].replace(".", "_")), csv_data_file_name)
 
                     csv_datafile_path = data_dir_file_path.joinpath("csv_files", csv_data_file_name)
@@ -52,7 +66,12 @@ def signal_converter_to_csv(path: str):
 
                         print("{} created".format(csv_datafile_path))
 
-                        datafile.export_csv(str(csv_datafile_path.resolve()))
+                        if not dry_run == True:
+
+                            datafile.export_csv(str(csv_datafile_path.resolve()))
+
+                        else:
+                            continue
 
             except(RuntimeError, TypeError, NameError):
                 print("experienced an error converting the .ch to .csv for {}".format(csv_datafile_path))
