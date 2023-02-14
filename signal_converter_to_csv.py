b4e01729efeafffa2ddb5dae1643b6bd2fefc3c8
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-02-14 15:15:23 +1100

    adds documentation and defines the signal_converter_to_csv module with type hinting

--- signal_converter_to_csv.py
+++ signal_converter_to_csv.py
@@ -1,54 +1,61 @@
+"""
+This module is to convert agilent .ch to .csv while remaining within the agilent DATA file heirarchy,
+adding the .csv files to a "csv_files" subdir in the .D dir.
+"""
 from pathlib import Path
 
 import rainbow as rb
 
 import re
 
-p = Path("/Users/jonathan/002_0_jono_data")
 
-data_dir_file_path_list = [x for x in p.iterdir() if ".D" in x.name]
+def signal_converter_to_csv(path: str):
 
-for data_dir_file_path in data_dir_file_path_list:
-    print("datadir file path:", data_dir_file_path)
+    p = Path(path)
 
-    for datadir in [rb.read(str(data_dir_file_path.resolve()))]:
-        
-        try:
-            
-            csv_folder_path = data_dir_file_path.joinpath("csv_files")
-            
-            if not csv_folder_path.exists():
-                
-                print("folder does not exist, creating {}".format(csv_folder_path))
+    data_dir_file_path_list = [x for x in p.iterdir() if ".D" in x.name]
 
-                csv_folder_path.mkdir()
-                      
-            if csv_folder_path.exists():
-                print("{} already exists".format(csv_folder_path))
-                
-        except(RuntimeError, TypeError, NameError):
-            print("error in creating csv_files directory")
-            
-        try:
-                      
-            for datafile in datadir.datafiles:
-                csv_data_file_name = str(datafile).replace(".ch", ".csv")
+    for data_dir_file_path in data_dir_file_path_list:
+        print("datadir file path:", data_dir_file_path)
 
-                csv_data_file_name = re.sub("DAD1.", "{}".format(datafile.ylabels[0].replace(".", "_")), csv_data_file_name)
+        for datadir in [rb.read(str(data_dir_file_path.resolve()))]:
 
-                csv_datafile_path = data_dir_file_path.joinpath("csv_files", csv_data_file_name)
+            try:
 
-                if csv_datafile_path.exists():
-                    print("{} already exists".format(csv_datafile_path))
+                csv_folder_path = data_dir_file_path.joinpath("csv_files")
 
-                if not csv_datafile_path.exists():
+                if not csv_folder_path.exists():
 
-                    print("{} created".format(csv_datafile_path))
+                    print("folder does not exist, creating {}".format(csv_folder_path))
 
-                    datafile.export_csv(str(csv_datafile_path.resolve()))
+                    csv_folder_path.mkdir()
 
-        except(RuntimeError, TypeError, NameError):
-            print("experienced an error converting the .ch to .csv for {}".format(csv_datafile_path))
-            
-        finally:    
-            print("\n")
+                if csv_folder_path.exists():
+                    print("{} already exists".format(csv_folder_path))
+
+            except(RuntimeError, TypeError, NameError):
+                print("error in creating csv_files directory")
+
+            try:
+
+                for datafile in datadir.datafiles:
+                    csv_data_file_name = str(datafile).replace(".ch", ".csv")
+
+                    csv_data_file_name = re.sub("DAD1.", "{}".format(datafile.ylabels[0].replace(".", "_")), csv_data_file_name)
+
+                    csv_datafile_path = data_dir_file_path.joinpath("csv_files", csv_data_file_name)
+
+                    if csv_datafile_path.exists():
+                        print("{} already exists".format(csv_datafile_path))
+
+                    if not csv_datafile_path.exists():
+
+                        print("{} created".format(csv_datafile_path))
+
+                        datafile.export_csv(str(csv_datafile_path.resolve()))
+
+            except(RuntimeError, TypeError, NameError):
+                print("experienced an error converting the .ch to .csv for {}".format(csv_datafile_path))
+
+            finally:
+                print("\n")
