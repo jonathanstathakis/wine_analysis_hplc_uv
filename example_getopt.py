b511c0f7637df823cd4a2cf7c151cf16066df832
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-02-22 12:21:13 +1100

    tons of stuff, main purpose of this commit is to get it in the web so I can link to files, i.e. 2023-02-22_parsing_uv_files.ipynb

---
+++ example_getopt.py
@@ -0,0 +1,41 @@
+"""
+source: 
+
+https://opensourceoptions.com/blog/how-to-pass-arguments-to-a-python-script-from-the-command-line/#:~:text=In%20Python%2C%20arguments%20are%20passed,used%20to%20parse%20named%20arguments.
+"""
+
+import sys
+import getopt
+
+
+def myfunc(argv):
+    arg_input = ""
+    arg_output = ""
+    arg_user = ""
+    arg_help = "{0} -i <input> -u <user> -o <output>".format(argv[0])
+    
+    try:
+        opts, args = getopt.getopt(argv[1:], "hi:u:o:", ["help", "input=", 
+        "user=", "output="])
+    except:
+        print(arg_help)
+        sys.exit(2)
+    
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print(arg_help)  # print the help message
+            sys.exit(2)
+        elif opt in ("-i", "--input"):
+            arg_input = arg
+        elif opt in ("-u", "--user"):
+            arg_user = arg
+        elif opt in ("-o", "--output"):
+            arg_output = arg
+
+    print('input:', arg_input)
+    print('user:', arg_user)
+    print('output:', arg_output)
+
+
+if __name__ == "__main__":
+    myfunc(sys.argv)
