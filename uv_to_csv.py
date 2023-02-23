a125ddb48b0af70a1eae752c3a60edc1ce76a2fe
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-02-24 09:56:18 +1100

    adds uv_to_csv, a UI wrapper around rainbow-api for converting .UV to .csv, and optimising_3d_spectra, a prototyped wrapper around plotly express for plotting the .UV data
    Former-commit-id: eb16bccda661813eb7d2da917076d767be1d7db2

---
+++ uv_to_csv.py
@@ -0,0 +1,31 @@
+import rainbow as rb
+
+def uv_to_csv(in_path = str, out_path = str):
+
+    print("in_path is: {}".format(in_path))
+    
+    uv_data = rb.agilent.chemstation.parse_uv(in_path)
+        
+    print("shape of the uv data is: {}".format(uv_data.data.shape))
+
+            # Convert the data to csv
+           
+    try:
+        uv_data.export_csv(out_path)
+        print("csv of UV data exported to: {}".format(out_path))
+        
+    except Exception as e:
+        print("export error {}, try again".format(e))
+
+def main():
+    
+    # uv_to_csv(in_path = input("inpath:"), out_path = input("outpath:"))
+    
+    uv_to_csv(in_path = input("inpath: "), out_path = input("outpath: "))
+    
+main()
+
+# /Users/jonathan/0_jono_data/2023-02-14_0052_TESTING_COLUMN_FOR_SAMPLE_DEG.D/DAD1.UV
+
+# /Users/jonathan/0_jono_data/2023-02-14_0052_TESTING_COLUMN_FOR_SAMPLE_DEG.D/DAD1.csv
+
