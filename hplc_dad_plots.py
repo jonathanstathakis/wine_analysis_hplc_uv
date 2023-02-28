ddf45a2f85702820fc796c523983b16bc43728d8
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-02-28 11:10:49 +1100

    mass commit, done a bunch of data structuring, UI, plotting work both in scripts and notebooks
    Former-commit-id: a9bfee39e59ce629d3f0ff4d306ef9e75e666e27
    Former-commit-id: 43d3800a9f5a29fa41fa793da31f0b3b8378365d

---
+++ hplc_dad_plots.py
@@ -0,0 +1,45 @@
+def plot_3d(uv_data, plot_title):
+    
+    """
+    Takes a wide format uv data df with mins as column 1 and a default index, melts it into a long format then plots a 3d line plot.
+    """
+    
+    # Set up the environment
+
+    import rainbow as rb
+
+    from pathlib import Path
+
+    import pandas as pd
+
+    from plotly import __version__ 
+
+    from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+    from plotly import express as px
+
+    melt_uv_df = uv_data.melt(id_vars = "mins")
+    
+    try:
+
+        melt_uv_df.columns = ['mins', 'nm', 'mAU']
+
+        melt_uv_df['nm'] = pd.to_numeric(melt_uv_df['nm'])
+    
+    except Exception as e:
+        print(e)
+
+    fig = px.line_3d(melt_uv_df,
+                     x = 'nm',
+                     y = 'mins',
+                     z = 'mAU',
+                     color = 'nm',
+                     title = plot_title,
+                     template = 'plotly_dark'
+                    )
+
+    fig.update_layout(width = 800, height = 800)
+    
+    fig.update_traces(line=dict(width=4))
+
+    display(fig)
