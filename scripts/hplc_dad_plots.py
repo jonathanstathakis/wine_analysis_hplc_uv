71811a8289e0754dd24667fcbb5d94c253b76ea3
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-02-28 19:50:33 +1100

    adds 2023-02-28 notebooks and a peak finder plot with associated functions in hplc_dad_plots.py

--- scripts/hplc_dad_plots.py
+++ scripts/hplc_dad_plots.py
@@ -43,3 +43,37 @@ def plot_3d_line(uv_data, plot_title):
     fig.update_traces(line=dict(width=4))
 
     display(fig)
+    
+import pandas as pd
+
+from pathlib import Path
+    
+import plotly.graph_objs as go
+
+from scipy.signal import find_peaks
+
+def peak_plot(data = pd.DataFrame, nm = int, plot_title = str):
+
+    peak_idx, peak_heights = find_peaks(data[nm], height = 50, distance = 50)
+
+    cx = data[nm].index.values
+
+    cy = data[nm].values
+
+    px = data.index.values[peak_idx]
+
+    py = data[nm].values[peak_idx] 
+
+    fig = go.Figure()
+
+    fig.update_layout(title = f"{plot_title}, {nm}")
+
+    peak_trace = go.Scatter(x = px, y = py, mode = 'markers', name = 'peaks')
+
+    chrom_trace = go.Scatter(x = cx, y = cy, mode = 'lines', name = 'chromatogram')
+
+    fig.add_trace(chrom_trace)
+
+    fig.add_trace(peak_trace)
+
+    fig.show()
