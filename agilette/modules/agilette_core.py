2b8da76fae4dc0814c375f3c9bf092187f0eae40
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-03-29 18:46:48 +1100

    moved Sequence to its own file, updated import statements

--- agilette/modules/agilette_core.py
+++ agilette/modules/agilette_core.py
@@ -28,29 +28,6 @@ import plotly.graph_objs as go
 from scripts.core_scripts.data_interface import retrieve_uv_data
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
-
-# class Sequence:
-#     """
-#     Currently just going to contain the data files.
-
-#     todo: add start dates.
-#     """
-#     def __init__(self, path: str):
-#         self.path = path
-#         self.data_files = self.data_files()
-
-#     def data_files(self):
-        
-#         try:
-#             data_file_dict = {Run_Dir(x).name : Run_Dir(x) for x in self.path.iterdir() if x.name.endswith(".D")}
-                    
-#         except Exception as e:
-#             print(f"{e}")
-        
-#         return data_file_dict
-        
-#     def __str__(self):
-#         return f"{self.path.name}, {self.data_files.keys()}"
     
 class UV_Data:
     """
