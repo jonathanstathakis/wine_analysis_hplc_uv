9ea4026ca6e5c41a72dba6156f2099e1547bcf50
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-03-29 20:04:10 +1100

    to provide a readable list of loaded runs in the Library class, have added a Runs_List class with a defined print behavior, and a test to verify it.
    Former-commit-id: 989f2ef63ce59c18616609d7bbb517ae7f4571cc
    Former-commit-id: 881658e4613886b167ae9fdb629f7f2b148914e8

--- agilette/tests/testing_loading_runs.py
+++ agilette/tests/testing_loading_runs.py
@@ -1,9 +1,13 @@
-from agilette.library import Library
+from agilette.modules.library import Library
 from pathlib import Path
 
 def testing_library() -> None:
 
-    path = Path('/Users/jonathan/0_jono_data/2023-02-23_2021-DEBORTOLI-CABERNET-MERLOT_AVANTOR.D')
+    # path = Path('/Users/jonathan/0_jono_data/2023-02-23_2021-DEBORTOLI-CABERNET-MERLOT_AVANTOR.D')
+
+    path = ['/Users/jonathan/0_jono_data/2023-02-23_2021-DEBORTOLI-CABERNET-MERLOT_AVANTOR.D','/Users/jonathan/0_jono_data/2023-02-22_KOERNER-NELLUCIO-02-21.D', '/Users/jonathan/0_jono_data/2023-02-16_0291.D']
 
     lib = Library(path)
+
+    print(lib.runs_list)
 testing_library()
