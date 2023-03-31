bc024d098202c27c9f4f5ffaddcc4acb25337e54
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-04-01 00:56:35 +1100

    ADD a method of opening a dataframe in a browser as  a html from a python script
    Former-commit-id: aded8c410ef18242a779a9e2ff3cc687bce85e13
    Former-commit-id: 55e1ac26edc5c0e1ef6061690c2f24e5b86b865a

--- notebooks/testing_library_new_data_2023-03-31.py
+++ notebooks/testing_library_new_data_2023-03-31.py
@@ -1,7 +1,15 @@
 import sys
+import webbrowser
+import os
 
 sys.path.append('../')
 
 from agilette.modules.library import Library
 
 lib = Library('/Users/jonathan/0_jono_data')
+
+out_path = 'df.html'
+
+lib.metadata_table.to_html(out_path)
+
+webbrowser.open('file:///' + os.getcwd() + '/' + out_path)
