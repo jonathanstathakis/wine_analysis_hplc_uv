96f17f7db4ad2592e319b6eecb1d12395e9d2133
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-04-10 16:12:13 +1000

    ADD baseline calculator for a given signal.

--- prototype_code/chrom_quality_analyser.py
+++ prototype_code/chrom_quality_analyser.py
@@ -24,28 +24,42 @@ import numpy as np
 from pybaselines import Baseline
 from scipy.signal import  find_peaks
 from agilette.modules.metadata_sampletracker_cellartracker_join import agilent_sample_tracker_cellartracker_super_table as super_table
-
 from agilette.modules.library import Library
-
 from agilette.modules.run_dir import Run_Dir
+import matplotlib.pyplot as plt
+
+
+import streamlit as st
 
 def main():
     #df = super_table()
     lib = Library(Path('/Users/jonathan/0_jono_data/2023-02-22_STONEY-RISE-PN_02-21.D'))
     lib = lib.load_spectrum()
 
+    # extract single wavelength from spectrum.
     def nm_extractor(spectrum : pd.DataFrame, nm : str) -> pd.DataFrame :
         """
-        Take a spectrum df and extract one wavlength, returning as a df of ['mins', 'mAU'].
+        Take a spectrum df and extract one wavelength, returning as a df of ['mins', 'mAU'].
         """
-
         nm_df = spectrum.loc[:,['mins', nm]]
-
         return nm_df
 
-    lib['nm_254 series'] = pd.Series(lib.apply(lambda x : nm_extractor(x['spectrum'],'254'), axis = 1))
+    # apply nm_extractor to each row of lib['spectrum'], a series of df's.
+    lib['nm_254'] = pd.Series(lib.apply(lambda row : nm_extractor(row['spectrum'],'254'), axis = 1))
 
+    # fit the baseline to that wavelength.
+    def signal_baseline_creator(signal_df : pd.DataFrame) -> pd.DataFrame:
+        """
+        Create baseline obj then fit the baseline on it. return baseline as df of ['mins','mAU']
+        """
+        baseline_obj = Baseline(signal_df['mins'])
+        baseline_y = baseline_obj.iasls(signal_df['254'])[0]
+        baseline_df = signal_df[['mins']].copy(deep = True)
+        baseline_df['baseline_254'] = baseline_y
+        return baseline_df
     
+    # create a baseline column.
+    lib['baseline_254'] = pd.Series(lib.apply(lambda row : signal_baseline_creator(row['nm_254']), axis = 1))
 
 main()
 
