c83a1a0c7454511b2e09e86bf6aa3d1007d62a0b
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-04-14 09:25:02 +1000

    EDIT moved the streamlit app into its own file because it was running the app on imports of super_table
    Former-commit-id: 40a4226332ebeb19aae5d3aa37955e08be347d71
    Former-commit-id: 47279164d6a20979b135ac56fcf3ed7f5b8819d3

--- agilette/modules/metadata_sampletracker_cellartracker_join.py
+++ agilette/modules/metadata_sampletracker_cellartracker_join.py
@@ -1,6 +1,8 @@
 """
 Joins `agilette.Library.metadata_table` with metadata from sample_tracker and cellartracker.
 
+2023-04-13-15-40: Import agilent_sample_tracker_cellartracker_super_table to get the subst of MRes thesis usable runs.
+
 See Users/jonathan/001_obsidian_vault/mres_logbook/2023-04-06_logbook.md, Users/jonathan/001_obsidian_vault/mres_logbook/2023-04-05_logbook_depicting-progress-thus-far, notebooks/2023-04-05_description-of-dataset-thus-far.ipynb for need, notebooks/2023-03-28-joining-cellartracker-metadata.ipynb for prototype.
 
 1. [x] get sample_tracker table.
@@ -18,7 +20,6 @@ import os
 from cellartracker import cellartracker
 import html
 from fuzzywuzzy import fuzz, process
-import streamlit as st
 import numpy as np
 
 def string_id_to_digit(df : pd.DataFrame) -> pd.DataFrame:
@@ -40,9 +41,7 @@ def selected_avantor_runs(df : pd.DataFrame) -> pd.DataFrame:
     Selects runs to be included in study dataset.
     """
     # select runs from 2023 on the avantor column.
-    print(df.shape)
     df = df[(df['acq_date'] > '2023-01-01') & (df['acq_method'].str.contains('avantor'))]
-    print(df.shape)
 
     sequences_to_drop = \
         list(df.groupby('sequence_name').filter(lambda x: len(x) == 1).groupby('sequence_name').groups.keys())\
@@ -69,7 +68,7 @@ def sample_tracker_df_builder():
     df = get_sheets_values_as_df(
         spreadsheet_id='15S2wm8t6ol2MRwTzgKTjlTcUgaStNlA22wJmFYhcwAY',
         range='sample_tracker!A1:H200',
-        creds_parent_path="/Users/jonathan/wine_analysis_hplc_uv/agilette/modules/credientals_tokens/")
+        creds_parent_path=os.environ.get('GOOGLE_SHEETS_API_CREDS_PARENT_PATH'))
     return df
 
 def library_id_replacer(df : pd.DataFrame) -> pd.DataFrame:
@@ -95,7 +94,7 @@ def agilette_library_loader():
         df = Library('/Users/jonathan/0_jono_data').metadata_table
         df.to_csv(file_path, index = False)
     else:
-        df = pd.read_csv(file_path, infer_datetime_format=True)
+        df = pd.read_csv(file_path)
 
         df['acq_date'] = pd.to_datetime(df['acq_date'])
     
@@ -111,7 +110,6 @@ def agilette_library_loader():
                     })
     
     df = df_string_cleaner(df)
-
     df = library_id_replacer(df)
 
     return df
@@ -142,24 +140,6 @@ def get_cellar_tracker_table():
     cellar_tracker_df = df_string_cleaner(cellar_tracker_df)
 
     return cellar_tracker_df
-
-def st_df_info(df : pd.DataFrame) -> pd.DataFrame:
-    
-    info_df = pd.DataFrame()
-    info_df['types'] = df.dtypes
-    info_df['NA'] = df.isnull().sum()
-
-    st.title(df.attrs['name'])
-    st.header('df shape and size')
-    st.write(f"shape: {df.shape} | size: {df.size}")
-
-    with st.container():
-        st.header('df columns')
-        st.table(info_df.astype(str))
-            
-    with st.container():
-        st.header(f"{df.attrs['name']} head")
-        st.write(df.head().astype(str))
 
 def sample_tracker_download():
     """
@@ -213,7 +193,7 @@ def join_dfs_with_fuzzy(df1 : pd.DataFrame, df2 : pd.DataFrame) -> pd.DataFrame:
 def agilent_sample_tracker_cellartracker_super_table():
     metadata_df = agilette_library_loader()
     metadata_df.attrs['name'] = 'metadata in attrs'
-    st_df_info(metadata_df)
+    #st_df_info(metadata_df)
 
     avantor_df = selected_avantor_runs(metadata_df)
     avantor_df = four_digit_id_to_two_digit(avantor_df)
@@ -222,15 +202,15 @@ def agilent_sample_tracker_cellartracker_super_table():
     sample_tracker_df = sample_tracker_download()
     sample_tracker_df.attrs['name'] = 'sample tracker'
     sample_tracker_df = sample_tracker_df[['id','vintage', 'name', 'open_date', 'sample_date', 'notes']]
-    st_df_info(sample_tracker_df)
+    #st_df_info(sample_tracker_df)
 
     merge_metadata_sample_tracker = pd.merge(avantor_df, sample_tracker_df, on ='id', how = 'left')
     merge_metadata_sample_tracker.attrs['name'] = 'metadata, sample tracker merge table'
-    st_df_info(merge_metadata_sample_tracker)
+    #st_df_info(merge_metadata_sample_tracker)
 
     cellartracker_df = get_cellar_tracker_table().convert_dtypes()
     cellartracker_df.attrs['name'] = 'cellar tracker table'
-    st_df_info(cellartracker_df)
+    #st_df_info(cellartracker_df)
     
     def form_join_col(df):
         df['join_key'] = df['vintage'] + " " + df['name']
@@ -241,15 +221,18 @@ def agilent_sample_tracker_cellartracker_super_table():
     
     super_table = join_dfs_with_fuzzy(merge_metadata_sample_tracker, cellartracker_df)
     super_table.attrs['name'] = 'final table'
-    st_df_info(super_table)
 
-    st.subheader('super table\njoin_key_matched missing values')
-    super_table[super_table['join_key_matched'].isna()]
+    print('hey')
+    print(super_table.shape)
+    # st_df_info(super_table)
 
-    st.subheader('Super Table')
-    super_table[['acq_date','id','join_key_similarity','join_key_ms', 'join_key_matched']]
+    # st.subheader('super table\njoin_key_matched missing values')
+    # super_table[super_table['join_key_matched'].isna()]
 
-    st.subheader('cellartracker debortoli')
-    cellartracker_df[cellartracker_df['name'].str.contains('debortoli')]
+    # st.subheader('Super Table')
+    # super_table[['acq_date','id','join_key_similarity','join_key_ms', 'join_key_matched']]
+
+    # st.subheader('cellartracker debortoli')
+    # cellartracker_df[cellartracker_df['name'].str.contains('debortoli')]
 
     return super_table
