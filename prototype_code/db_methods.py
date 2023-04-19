51a578cb6647c5082d02c50acf9b4a32e08303e6
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-04-19 16:29:16 +1000

    ADD general df cleaning and db read/write methods for use across the project.
    Former-commit-id: 2807c3c0e703a0f31598fd7fcd202b92b4185daf
    Former-commit-id: 191ea03fd7458cb3b4bd345252e50d2fca23703b

---
+++ prototype_code/db_methods.py
@@ -0,0 +1,33 @@
+""" 
+A file to contain general duckdb database methods.
+"""
+import pandas as pd
+import duckdb as db
+def display_table_info(con : db.DuckDBPyConnection, table_name : str) -> None:
+    print(f"\n\n###### {table_name.upper()} TABLE ######\n")
+    con.sql(f"DESCRIBE TABLE {table_name}").show()
+    con.sql(f"SELECT COUNT(*) FROM {table_name}").show()
+    con.sql(f"SELECT * FROM {table_name} LIMIT 5").show()
+
+def write_df_to_table(df : pd.DataFrame, con : db.DuckDBPyConnection, table_name : str, schema : str, table_column_names : str, df_column_names : str) -> None:
+
+    con.sql(f"""
+    DROP TABLE IF EXISTS {table_name};
+    CREATE TABLE {table_name} ({schema});
+    """)    
+    
+    insert_query = f"""
+            INSERT INTO {table_name} (
+            {table_column_names}
+            )
+            SELECT
+            {df_column_names}
+            FROM df;
+            """
+    try:
+        con.sql(insert_query)
+    except Exception as e:
+        print(e)
+        print(f"full query is:\n\n{insert_query}")
+    
+    display_table_info(con, table_name)
