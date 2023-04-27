de941566cd5c5c4ee98b45ed00a0f1c8b436e660
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-04-27 22:10:50 +1000

    ADD printing of progress reports to help with debugging.

--- prototype_code/df_cleaning_methods.py
+++ prototype_code/df_cleaning_methods.py
@@ -6,9 +6,10 @@ import pandas as pd
 def df_string_cleaner(df : pd.DataFrame) -> pd.DataFrame:
         def lowercase_and_strip(value):
                 if isinstance(value, str):
+
                         return value.lower().strip()
                 return value
-
+        print('lowering case and stripping all str columns in df')
         df = df.applymap(lowercase_and_strip)
         
         return df
