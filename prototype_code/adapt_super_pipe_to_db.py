bf000b7d169a590a1807fe64f81e756a980a2707
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-04-27 22:09:36 +1000

    FIX added type control to cellartracker df init from database which was causing join errors due to not being able to handle the default nan format from db. forcing Int64 dtype on vintage now.

--- prototype_code/adapt_super_pipe_to_db.py
+++ prototype_code/adapt_super_pipe_to_db.py
@@ -5,6 +5,7 @@ import duckdb as db
 import pandas as pd
 import db_methods as db_m
 from function_timer import timeit
+import numpy as np
 
 @timeit
 def db_super_pipe():
@@ -13,20 +14,21 @@ def db_super_pipe():
         con.sql("drop table if exists super_table")
 
         chemstation_df = con.sql('SELECT * FROM cleaned_chemstation_metadata').df()
-
         sample_tracker_df = con.sql('SELECT * FROM cleaned_sample_tracker').df()
-
         cellartracker_df = con.sql('SELECT * FROM cleaned_cellartracker').df()
+
+        cellartracker_df.vintage = cellartracker_df.vintage.astype('Int64')
+        
+        print(cellartracker_df[cellartracker_df['vintage'] == 'empty'])
 
         print(f'Starting with {chemstation_df.shape[0]} rows in chemstation_df')
 
         super_table_df = super_table_pipe(chemstation_df, sample_tracker_df, cellartracker_df)
 
-        create_super_table(super_table_df)
+        create_super_table(super_table_df, con)
 
 def create_super_table(df : pd.DataFrame, con:db.DuckDBPyConnection) -> None:
-    con.sql("CREATE TABLE super_table AS SELECT * FROM super_table")
-
+    con.sql("CREATE TABLE super_table AS SELECT * FROM df")
     con.sql("SELECT * FROM super_table").show()
 
 def main():
