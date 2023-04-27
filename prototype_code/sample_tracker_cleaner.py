6e3a7e2e4358da57142ea3273ff4fbd369719d90
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-04-27 22:12:27 +1000

    FIX removed duplicate call to display_table_info causing duplicate report during runtime. ADD return None statements after functions to make it easier to see where the scope ends. ADD more verbose print statements during progress to aid in debugging.

--- prototype_code/sample_tracker_cleaner.py
+++ prototype_code/sample_tracker_cleaner.py
@@ -10,21 +10,25 @@ from df_cleaning_methods import df_string_cleaner
 from db_methods import display_table_info, write_df_to_table
 
 def init_cleaned_sample_tracker_table(con : db.DuckDBPyConnection, raw_table_name : str) -> None:
-    
+
+    print('generating raw_sample_tracker_table from db')    
     raw_sample_tracker_df = con.sql(f"SELECT * FROM {raw_table_name}").df()
-    
+
     cleaned_sample_tracker_df = sample_tracker_df_cleaner(raw_sample_tracker_df)
     
     out_name = raw_table_name.replace('raw','cleaned')
 
     write_clean_sample_tracker_to_db(cleaned_sample_tracker_df, con, out_name)
 
+    return None
+
 def sample_tracker_df_cleaner(df):
+    print('cleaning raw_sample_tracker_df')
     try:
         df = df_string_cleaner(df)
         df.columns = df.columns.str.lower()
-    except:
-        print(df.dtypes)
+    except Exception as e:
+        print(f'while cleaning raw_sample_tracker_df, encountered Exception: {e}')
 
     return df
 
@@ -53,7 +57,7 @@ def write_clean_sample_tracker_to_db(df, con, table_name) -> None:
     
     write_df_to_table(df, con, table_name, schema, table_column_names = col_names, df_column_names = col_names)
 
-    display_table_info(con, table_name)
+    return None
 
 def main():
     sample_tracker_df_cleaner()
