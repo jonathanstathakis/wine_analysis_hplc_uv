be350a8d5a142223b5cde153bec1669cda008c1c
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-04-27 22:11:19 +1000

    FIX made try except exception print statement more succinct.
    Former-commit-id: 7aa202ae3810e1daf67b8d0c8106f05294490c9f
    Former-commit-id: 821aedf45517ff2536c767cd09b3c9258efb5355

--- agilette/modules/metadata_sampletracker_cellartracker_join.py
+++ agilette/modules/metadata_sampletracker_cellartracker_join.py
@@ -140,8 +140,11 @@ def join_dfs_with_fuzzy(df1 : pd.DataFrame, df2 : pd.DataFrame) -> pd.DataFrame:
     def fuzzy_match(s1, s2):
         return fuzz.token_set_ratio(s1, s2)
 
-    df1 = df1.fillna('empty')
-    df2 = df2.fillna('empty')
+    try:
+        df1 = df1.fillna('empty')
+        df2 = df2.fillna('empty')
+    except Exception as e:
+        print(f'tried to fill empties in both dfs but {e}')
 
     df1['join_key_match'] = df1['join_key'].apply(lambda x: process.extractOne(x, df2['join_key'], scorer = fuzzy_match))
 
