760733f3c83fc1acdb29e6176e689c03657dbefc
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-04-28 12:38:10 +1000

    EDIT moved signal treatment methods developed in chrom_quality_analyser to signal_data_treatment_methods for later use. Added import statements to get them back into chrom_quality_analyser

--- prototype_code/chrom_quality_analyser.py
+++ prototype_code/chrom_quality_analyser.py
@@ -21,14 +21,13 @@ import sys
 sys.path.append('../')
 import pandas as pd
 import numpy as np
-from pybaselines import Baseline
-from scipy.signal import  find_peaks
 from agilette.modules.metadata_sampletracker_cellartracker_join import agilent_sample_tracker_cellartracker_super_table as super_table
 from agilette.modules.library import Library
 from agilette.modules.run_dir import Run_Dir
 import plotly.graph_objects as go
 import streamlit as st
 import inspect
+import signal_data_treatment_methods as dt
 
 st.set_page_config(layout = 'wide')
 
@@ -49,38 +48,9 @@ def nm_extractor(spectrum : pd.DataFrame, nm : str) -> pd.DataFrame:
     nm_df = test_df_empty(nm_df,inspect.currentframe().f_code.co_name)
     return nm_df
 
-def signal_baseline_creator(signal_df : pd.DataFrame) -> pd.DataFrame:
-    """
-    Create baseline obj then fit the baseline on it. return baseline as df of ['mins','mAU']
-    """
-    baseline_obj = Baseline(signal_df['mins'])
-    baseline_y = baseline_obj.iasls(signal_df['254'])[0]
-    baseline_df = signal_df[['mins']].copy(deep = True)
-    baseline_df['mAU'] = baseline_y
-    return baseline_df
-
-    
-def baseline_area(df : pd.DataFrame) -> float:
-    """
-    calculate area under the baseline curve
-    """
-    area = np.trapz(y = df['mAU'], x = df['mins'])
-    return area
-
-def peak_finder(signal_df : pd.DataFrame, in_height = None, in_prominence = None) -> pd.DataFrame:
-    """
-    find peaks for a given signal df, returned as a df of ['mins', 'mAU']
-    """
-    peak_idx, peak_y = find_peaks(signal_df['254'], height = in_height, prominence = in_prominence)
-    peak_y = peak_y['peak_heights']
-    peak_x = signal_df['mins'][peak_idx]
-    peak_df = pd.DataFrame(zip(peak_x, peak_y), columns = ['mins', 'mAU'])
-    print(peak_df.shape)
-    return peak_df
-
 def update_peak_trace(lib, prominence_slider):
         
-    new_peaks = lib.apply(lambda row : peak_finder(row['nm_254'], 0.05, in_prominence = prominence_slider), axis = 1)
+    new_peaks = lib.apply(lambda row : dt.peak_finder(row['nm_254'], 0.05, in_prominence = prominence_slider), axis = 1)
     new_x = new_peaks[0]['mins']
     new_y = new_peaks[0]['mAU']
     
@@ -114,16 +84,16 @@ def main():
     lib['nm_254']
     # create a baseline column.
     # fit the baseline to that wavelength.
-    lib['baseline_254'] = pd.Series(lib.apply(lambda row : signal_baseline_creator(row['nm_254']), axis = 1))
+    lib['baseline_254'] = pd.Series(lib.apply(lambda row : dt.signal_baseline_creator(row['nm_254']), axis = 1))
 
     # create baseline AUC column
-    lib['baseline_254_area'] = pd.Series(lib.apply(lambda row :  baseline_area(row['baseline_254']), axis = 1))
+    lib['baseline_254_area'] = pd.Series(lib.apply(lambda row :  dt.baseline_area(row['baseline_254']), axis = 1))
 
     # calculate peak prominances
     # to do this will need to locate the peaks and provide them to peak_prominances as a 'sequence', i.e. list or series.
     # 1. Locate peaks
     # identify peaks using scipy.signal.find_peaks
-    lib['peaks_254'] = lib.apply(lambda row : peak_finder(row['nm_254'], 0.05, 2), axis = 1)
+    lib['peaks_254'] = lib.apply(lambda row : dt.peak_finder(row['nm_254'], 0.05, 2), axis = 1)
 
     streamlit_peak_finding(lib['nm_254'][0], lib['baseline_254'], lib['peaks_254'])
         
---
+++ prototype_code/signal_data_treatment_methods.py
@@ -0,0 +1,36 @@
+"""
+A file to contain all the general data treatment methods: baseline correction, mean centering, normalization, peak alignment, peak finding, etc.
+"""
+import pandas as pd
+from pybaselines import Baseline
+import numpy as np
+from scipy.signal import  find_peaks
+
+def signal_baseline_creator(signal_df : pd.DataFrame) -> pd.DataFrame:
+    """
+    Create baseline obj then fit the baseline on it. return baseline as df of ['mins','mAU']
+    """
+    baseline_obj = Baseline(signal_df['mins'])
+    baseline_y = baseline_obj.iasls(signal_df['254'])[0]
+    baseline_df = signal_df[['mins']].copy(deep = True)
+    baseline_df['mAU'] = baseline_y
+    return baseline_df
+
+    
+def baseline_area(df : pd.DataFrame) -> float:
+    """
+    calculate area under the baseline curve
+    """
+    area = np.trapz(y = df['mAU'], x = df['mins'])
+    return area
+
+def peak_finder(signal_df : pd.DataFrame, in_height = None, in_prominence = None) -> pd.DataFrame:
+    """
+    find peaks for a given signal df, returned as a df of ['mins', 'mAU']
+    """
+    peak_idx, peak_y = find_peaks(signal_df['254'], height = in_height, prominence = in_prominence)
+    peak_y = peak_y['peak_heights']
+    peak_x = signal_df['mins'][peak_idx]
+    peak_df = pd.DataFrame(zip(peak_x, peak_y), columns = ['mins', 'mAU'])
+    print(peak_df.shape)
+    return peak_df
