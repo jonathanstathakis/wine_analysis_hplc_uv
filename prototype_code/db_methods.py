deb5ec88f74aa3ac8b78fc5d388e5c64319f43a9
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-04-28 13:13:46 +1000

    ADD get_spectrums method which fetches spectrums based on a hash_key in the metadata table.

--- prototype_code/db_methods.py
+++ prototype_code/db_methods.py
@@ -3,6 +3,13 @@ A file to contain general duckdb database methods.
 """
 import pandas as pd
 import duckdb as db
+
+def index():
+    display_table_info()
+    write_df_to_table()
+    #list_table_names_not_spectrum()
+    get_spectrums()
+
 def display_table_info(con : db.DuckDBPyConnection, table_name : str) -> None:
     print(f"\n\n###### {table_name.upper()} TABLE ######\n")
     con.sql(f"DESCRIBE TABLE {table_name}").show()
@@ -35,3 +42,28 @@ def write_df_to_table(df : pd.DataFrame, con : db.DuckDBPyConnection, table_name
         print(f"full query is:\n\n{insert_query}")
     
     display_table_info(con, table_name)
+
+# def list_table_names_not_spectrum(con : db.DuckDBPyConnection) -> None:
+#     query = """
+#     SELECT table_name from information_schema.tables
+#     WHERE table_type = 'BASE TABLE'
+#     and table_name NOT LIKE '%hplc_spectrum%';
+#     """
+#     con.sql(query).show()
+
+def get_spectrums(metadata_df : pd.DataFrame, con : db.DuckDBPyConnection) -> pd.DataFrame:
+        """
+        Pass a wine metadata dataframe with relevant hash_key to join to spectrums contained in spectrum table found through the con object. returns the metadata df with a spectrum column, where spectrums are nested dataframes.
+        """
+        def get_spectrum(con : db.DuckDBPyConnection, hash_key : str) -> pd.DataFrame:
+            query = f"""
+            SELECT * from spectrums
+            where hash_key = '{hash_key}'
+            """
+            df = con.sql(query).df()
+            return df
+
+        metadata_df['spectrum'] = metadata_df.apply(lambda row : get_spectrum(con, row['hash_key']), axis = 1)
+        metadata_df = metadata_df.drop('hash_key', axis =1)
+
+        return metadata_df
