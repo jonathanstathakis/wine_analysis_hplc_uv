c2f8b2c59d856fd906be01568663bd756eae5a08
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-04-28 13:15:30 +1000

    Bunch of edits to make the pipe more modular and easier to import(?)

--- prototype_code/adapt_super_pipe_to_db.py
+++ prototype_code/adapt_super_pipe_to_db.py
@@ -6,33 +6,34 @@ import pandas as pd
 import db_methods as db_m
 from function_timer import timeit
 import numpy as np
+import db_methods
 
 @timeit
-def db_super_pipe():
-    with db.connect('wine_auth_db.db') as con:
+def load_super_table(con : db.DuckDBPyConnection):
 
-        con.sql("drop table if exists super_table")
+    con.sql("drop table if exists super_table")
 
-        chemstation_df = con.sql('SELECT * FROM cleaned_chemstation_metadata').df()
-        sample_tracker_df = con.sql('SELECT * FROM cleaned_sample_tracker').df()
-        cellartracker_df = con.sql('SELECT * FROM cleaned_cellartracker').df()
+    chemstation_df = con.sql('SELECT * FROM cleaned_chemstation_metadata').df()
+    sample_tracker_df = con.sql('SELECT * FROM cleaned_sample_tracker').df()
+    cellartracker_df = con.sql('SELECT * FROM cleaned_cellartracker').df()
 
-        cellartracker_df.vintage = cellartracker_df.vintage.astype('Int64')
-        
-        print(cellartracker_df[cellartracker_df['vintage'] == 'empty'])
+    cellartracker_df.vintage = cellartracker_df.vintage.astype('Int64')
+    
+    print(cellartracker_df[cellartracker_df['vintage'] == 'empty'])
 
-        print(f'Starting with {chemstation_df.shape[0]} rows in chemstation_df')
+    print(f'Starting with {chemstation_df.shape[0]} rows in chemstation_df')
 
-        super_table_df = super_table_pipe(chemstation_df, sample_tracker_df, cellartracker_df)
+    super_table_df = super_table_pipe(chemstation_df, sample_tracker_df, cellartracker_df)
 
-        create_super_table(super_table_df, con)
+    write_super_table_to_db(super_table_df, con)
 
-def create_super_table(df : pd.DataFrame, con:db.DuckDBPyConnection) -> None:
-    con.sql("CREATE TABLE super_table AS SELECT * FROM df")
-    con.sql("SELECT * FROM super_table").show()
+def write_super_table_to_db(df : pd.DataFrame, con:db.DuckDBPyConnection) -> None:
+    table_name = 'super_table'
+    con.sql(f"CREATE TABLE {table_name} AS SELECT * FROM df")
+    db_methods.display_table_info(con, table_name)
 
 def main():
-    db_super_pipe()
+    load_super_table()
 
 if __name__ == "__main__":
     main()
