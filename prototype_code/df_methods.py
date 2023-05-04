d3391bc90307f04a691c052e78f06f626245a4ce
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-05-05 00:12:15 +1000

    df methods to contain general pandas methods

--- prototype_code/df_methods.py
+++ prototype_code/df_methods.py
@@ -1,3 +1,16 @@
 # spectrum_df['signal_shape'] = spectrum_df.apply(lambda row : row[wavelength].shape, axis = 1)
 # spectrum_df['signal_index'] = spectrum_df.apply(lambda row : row[wavelength].index.tolist(), axis = 1)
 # spectrum_df['signal_columns'] = spectrum_df.apply(lambda row : row[wavelength].columns.tolist(), axis = 1)
+
+"""
+Contains general df methods to improve quality of life when working with pandas dataframes.
+"""
+
+import pandas as pd
+
+def describe_df(df : pd.DataFrame):
+    print("df shape:", df.shape)
+    print("df columns", df.columns)
+    print("df index", df.index)
+
+    return None
