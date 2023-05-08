40d334b6df3d0873e433a66e23108fc6750537c8
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-05-08 23:48:07 +1000

    ADD rudimentary peak finding plot functionality using plotly shapes. Color scheme sucks, markers suck, but it works.
    Former-commit-id: bc7aa1a5f5fe7be63cd2d07da73d7f70dee702dd
    Former-commit-id: dc8a0db1f4489a7e25027336074ce290df4b4502

--- prototype_code/plotly_plot_methods.py
+++ prototype_code/plotly_plot_methods.py
@@ -1,25 +1,89 @@
 from plotly import graph_objects as go
 import signal_data_treatment_methods as dt
 import pandas as pd
+import plotly.io as pio
+import numpy as np
 
 def plot_signal_in_series(series : pd.Series, x_key, y_key):
     """
     Take a series of dfs, where the dfs are organised as RangeIndex | mins | signal and return a plotly fig of each df overlaid.
     """
     fig = go.Figure()
-    for idx in series.index:
-        x = series[idx][x_key]
-        y = series[idx][y_key]
+
+    for sample_name in series.index:
+
+        data_df = series[sample_name]
+        x = data_df[x_key]
+        y = data_df[y_key]
     
-        # signals
-        fig.add_trace(go.Scatter(x = x, y = y, mode = 'lines', name = idx, text = idx))
 
         # peaks
-        peak_trace = get_trace_peak_maxima(series[idx])
-        peak_trace['text'] = idx
-        fig.add_trace(peak_trace)
+        peak_df = get_peak_df(data_df)
+        #peak_df['text'] = id
         
-    
+        # signals
+        fig.add_trace(go.Scatter(x = x, 
+                                 y = y, 
+                                 mode = 'lines',
+                                 name = sample_name,
+                                 text = sample_name,
+                                 opacity = 0.65))
+
+        peak_maxima_marker_ratio = 0.03
+        peak_maxima_marker_fill_color = 'red'
+
+        # add peak maxima trace
+        for idx, row in peak_df.reset_index(drop = True).iterrows():
+            xi, yi = row['peak_x'], row['peak_y']
+            peak_idx = idx + 1
+
+            customdata = np.array([[xi, yi]])
+
+            name = f'{sample_name} peak #{peak_idx}'
+            
+            fig.add_trace(go.Scatter(x = [xi],
+                                    y = [yi],
+                                    marker_symbol = 'circle',
+                                    mode = 'markers',
+                                    opacity = 0.0,
+                                    showlegend = False,
+                                    name = name,
+                                    hovertemplate=f'{name}:<br>{xi:.3f}, {yi:.3f}<extra></extra>'
+                                    ))
+            
+            x_range = x.max() - x.min()
+            y_range = y.max() - y.min()
+            
+
+            shape_size_x = x_range * peak_maxima_marker_ratio/4
+            shape_size_y = y_range * peak_maxima_marker_ratio*2
+
+            # forward slash
+            fig.add_shape(
+                        type = 'line',
+                          yref = 'y',
+                          x0 = xi - shape_size_x/2,
+                          x1 = xi + shape_size_x/2,
+                          y0 = yi - shape_size_y/2,
+                          y1 = yi + shape_size_y/2,
+                          line=dict(color=peak_maxima_marker_fill_color, width = 2),
+                          fillcolor = peak_maxima_marker_fill_color,
+                          opacity = 0.75
+                          )
+            
+            # back slash
+            fig.add_shape(
+                        type = 'line',
+                          yref = 'y',
+                          x0 = xi + shape_size_x/2,
+                          y0 = yi - shape_size_y/2,
+                          x1 = xi - shape_size_x/2,
+                          y1 = yi + shape_size_y/2,
+                          line=dict(color=peak_maxima_marker_fill_color, width = 2),
+                          fillcolor = peak_maxima_marker_fill_color,
+                          opacity = 0.75
+                          )
+            
     fig.update_layout(
         legend = dict(
         x = 0.5,
@@ -32,7 +96,7 @@ def plot_signal_in_series(series : pd.Series, x_key, y_key):
     
     return fig
 
-def get_trace_peak_maxima(df : pd.DataFrame) -> go.Scatter:
+def get_peak_df(df : pd.DataFrame) -> go.Scatter:
     """
     Take a dataframe signal of shape RangeIndex | mins | signal and return a plotly trace of that dataframe signal's peaks. To be used within plot_signal_in_series as option.
     """
@@ -41,8 +105,11 @@ def get_trace_peak_maxima(df : pd.DataFrame) -> go.Scatter:
     peak_df = dt.peak_finder(x = x, y = y, in_height = 20, in_prominence=5)
 
     peak_x, peak_y = peak_df[peak_df.columns[0]], peak_df[peak_df.columns[1]]
-    
-    peak_trace = go.Scatter(x=peak_x, y=peak_y, mode='markers', showlegend=False, name = 'peak maxima', marker_symbol = 'x')
 
-    return peak_trace
+    peak_marker_trace = go.Scatter(x = peak_x, y = peak_y, marker_symbol = 1)
+
+    # make this oen invisible
+    #peak_trace = go.Scatter(x=peak_x, y=peak_y, mode='markers', showlegend=False, name = 'peak maxima', marker_symbol = 'x')
+
+    return peak_df
     
