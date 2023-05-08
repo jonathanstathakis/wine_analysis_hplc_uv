6654a069789cb5d3a97f56038c2c5c1c9db728b4
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-05-08 18:46:58 +1000

    ADD superimpose peak maxima trace for each sample in intended singal plot
    Former-commit-id: c8ff7c67cd3174cb7a5ea225ec841e013ee70d55
    Former-commit-id: ac63d3581fbb35371363d8b7058f4aafc8f3eeaf

--- prototype_code/plot_methods.py
+++ prototype_code/plot_methods.py
@@ -1,10 +1,24 @@
 from plotly import graph_objects as go
+import signal_data_treatment_methods as dt
 import pandas as pd
 
 def plot_signal_in_series(series : pd.Series, x_key, y_key):
+    """
+    Take a series of dfs, where the dfs are organised as RangeIndex | mins | signal and return a plotly fig of each df overlaid.
+    """
     fig = go.Figure()
     for idx in series.index:
-        fig.add_trace(go.Scatter(x = series[idx][x_key], y = series[idx][y_key], mode = 'lines',name = idx, text = idx))
+        x = series[idx][x_key]
+        y = series[idx][y_key]
+    
+        # signals
+        fig.add_trace(go.Scatter(x = x, y = y, mode = 'lines', name = idx, text = idx))
+
+        # peaks
+        peak_trace = get_trace_peak_maxima(series[idx])
+        peak_trace['text'] = idx
+        fig.add_trace(peak_trace)
+        
     
     fig.update_layout(
         legend = dict(
@@ -15,23 +29,20 @@ def plot_signal_in_series(series : pd.Series, x_key, y_key):
         orientation = 'h',
         traceorder = 'normal'),
         margin = dict(b=100))
+    
     return fig
 
-def plot_signal_from_df_in_dict(dict_of_df : dict, x_key, y_key):
-    fig = go.Figure()
+def get_trace_peak_maxima(df : pd.DataFrame) -> go.Scatter:
+    """
+    Take a dataframe signal of shape RangeIndex | mins | signal and return a plotly trace of that dataframe signal's peaks. To be used within plot_signal_in_series as option.
+    """
+    x, y = df[df.columns[0]], df[df.columns[1]]
+    
+    peak_df = dt.peak_finder(x = x, y = y, in_height = 20, in_prominence=5)
 
-    print(dict_of_df)
-    for key, chromatogram_df in dict_of_df.items():
-        print(key)
-        fig.add_trace(go.Scatter(x = chromatogram_df[x_key], y = chromatogram_df[y_key], mode = 'lines', name = key, text = key))
-    fig.update_layout(
-        legend = dict(
-        x = 0.5,
-        y = -0.1,
-        xanchor = 'center',
-        yanchor = 'top',
-        orientation = 'h',
-        traceorder = 'normal'),
-        margin = dict(b=100)
-    )
-    return fig
+    peak_x, peak_y = peak_df[peak_df.columns[0]], peak_df[peak_df.columns[1]]
+    
+    peak_trace = go.Scatter(x=peak_x, y=peak_y, mode='markers', showlegend=False, name = 'peak maxima', marker_symbol = 'x')
+
+    return peak_trace
+    
