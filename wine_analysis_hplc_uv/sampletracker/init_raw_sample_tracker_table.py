4324c1f4496a3b169e77fb6b81e708c06738b5d4
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-05-14 19:37:17 +1000

    EDIT modified the functions of the process to pas db_filepath rather than con objecst. Moved table_display_info out of write_df_to_table, better to expose in main func.
    Former-commit-id: df07b52551494894d028fb2b2880df5fe10e1989

--- wine_analysis_hplc_uv/sampletracker/init_raw_sample_tracker_table.py
+++ wine_analysis_hplc_uv/sampletracker/init_raw_sample_tracker_table.py
@@ -1,20 +1,21 @@
+import pandas as pd
 from ..sampletracker import sample_tracker_methods
 from ..db_methods import db_methods
 import duckdb as db
 from ..google_sheets_api import google_sheets_api
 
 
-def init_raw_sample_tracker_table(con: db.DuckDBPyConnection) -> None:
+def init_raw_sample_tracker_table(db_filepath: str) -> None:
     # download the current sample tracker table
     df = sample_tracker_methods.sample_tracker_df_builder()
     df = df.replace({"": None})
     table_name = "raw_sample_tracker"
-    write_raw_sample_tracker_to_db(df, con, table_name)
-    db_methods.display_table_info(con, table_name)
+    write_raw_sample_tracker_to_db(df, db_filepath, table_name)
+    db_methods.display_table_info(db_filepath, table_name)
     return None
 
 
-def write_raw_sample_tracker_to_db(df, con, table_name):
+def write_raw_sample_tracker_to_db(df : pd.DataFrame, db_filepath: str, db_table_name: str):
     schema = """
         id INTEGER,
         vintage VARCHAR,
@@ -39,7 +40,10 @@ def write_raw_sample_tracker_to_db(df, con, table_name):
 
     column_assignment = target_columns
 
-    write_df_to_table(df, con, table_name, schema, target_columns, column_assignment)
+    assert isinstance(db_filepath, str)
+
+    db_methods.write_df_to_table(df, db_filepath, db_table_name, schema, target_columns, column_assignment)
+    db_methods.display_table_info(db_filepath, db_table_name)
 
 
 def main():
