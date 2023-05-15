423980072abc5b4319b85b12bbd0fbf524e7e249
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-05-15 15:57:38 +1000

    update dirpaths after rsetructure, organised import statements
    Former-commit-id: 057c4e1bab5cacfd2e46c39a51cb7cf457d41ac4
    Former-commit-id: 8d74ecb29bbc8a688ea3d14a449cf5ed2acd70bd
    Former-commit-id: 1a7db3b31bb0f5b31ede5cd1b123a929f3476d2b

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+from re import U
+
+
+U#!/usr/bin/env python3
 """
 The main file of the wine_analysis_hplc_uv thesis project. Will act
 as the overarching pipeline to get to final results.
 """
+import os
+
 from wine_analysis_hplc_uv.core import build_library
 from wine_analysis_hplc_uv.devtools import project_settings
 from wine_analysis_hplc_uv.signal_processing.peak_alignment import \
@@ -12,7 +18,7 @@ def core():
     """
     Project main file driver function. A pipe to go from start to finish.
     """
-    db_filepath, data_lib_path = "wine_auth_db.db", "/Users/jonathan/0_jono_data"
+    db_filepath, data_lib_path = os.environ.get("WINE_AUTH_DB_PATH"), "/Users/jonathan/0_jono_data"
     # Phase 1: collect and preprocess data
     build_library.build_db_library(db_filepath, data_lib_path)
 
