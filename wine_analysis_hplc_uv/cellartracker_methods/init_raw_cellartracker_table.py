135b119dc5cb6daa079e0a4e1c2a602de90f4181
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-05-16 12:47:44 +1000

    EDIT change init_raw_cellartracker_table args to db_filepath, db_table_name for top level control of vars, controlled connection times to db.
    Former-commit-id: 20412f6dbe2f5021a7e1b9fa8fa5a4c0ca972a7e
    Former-commit-id: 44ca2591055f33d30ff7d10b0cae171473a79779
    Former-commit-id: ea847b84672f772b6ba8a6e372948e7eb4730711

--- wine_analysis_hplc_uv/cellartracker_methods/init_raw_cellartracker_table.py
+++ wine_analysis_hplc_uv/cellartracker_methods/init_raw_cellartracker_table.py
@@ -9,18 +9,25 @@ from ..cellartracker_methods import download_cellartracker_table
 from ..db_methods import db_methods
 
 
-def init_raw_cellartracker_table(con):
+def init_raw_cellartracker_table(db_filepath: str, db_table_name: str):
     cellartracker_df = download_cellartracker_table.get_cellar_tracker_table()
 
-    table_name = "raw_cellartracker"
+    with db.connect(db_filepath) as con:
+        con.sql(f"DROP TABLE IF EXISTS {db_table_name}")
 
-    con.sql(f"DROP TABLE IF EXISTS {table_name}")
+        con.sql(
+            f"""
+            CREATE TABLE IF NOT EXISTS 
+                {db_table_name}
+            AS
+                SELECT 
+                    * 
+                FROM
+                    cellartracker_df
+            """
+        )
 
-    con.sql(
-        f"CREATE TABLE IF NOT EXISTS {table_name} AS SELECT * FROM cellartracker_df"
-    )
-
-    db_methods.display_table_info(con, table_name)
+    db_methods.display_table_info(db_filepath, db_table_name)
 
 
 def main():
