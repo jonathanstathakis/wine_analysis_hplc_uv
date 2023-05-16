6d5b3443bc85ae963467cc32d6f4cef7bbb0d49e
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-05-16 12:59:39 +1000

    formatting, removed unused imports
    Former-commit-id: 9f3fe5570b68ab5fca2f43a393dd8b1278d8aaf0

--- wine_analysis_hplc_uv/chemstation/chemstation_process_entry.py
+++ wine_analysis_hplc_uv/chemstation/chemstation_process_entry.py
@@ -28,22 +28,24 @@ to db are as follows:
  the list of files to process, calls the processor, passes the processed data to the pickler, passes the processed data to the db writing function.
 """
 import os
-import sys
-
 import duckdb as db
 import pandas as pd
-
-from ..chemstation import (chemstation_methods, chemstation_to_db_methods,
-                           pickle_chemstation_data)
+from ..chemstation import (
+    chemstation_methods,
+    chemstation_to_db_methods,
+    pickle_chemstation_data,
+)
 from ..db_methods import db_methods
 from ..devtools import function_timer as ft
 from ..devtools import project_settings
 
 
-def chemstation_data_to_db(data_lib_path: str,
-                           db_filepath: str,
-                           raw_chemstation_metadata_table_name: str,
-                           raw_chemstation_spectra_table_name: str):
+def chemstation_data_to_db(
+    data_lib_path: str,
+    db_filepath: str,
+    raw_chemstation_metadata_table_name: str,
+    raw_chemstation_spectra_table_name: str,
+):
     """
     main driver file, handle any preprocessing then activate write_ch_metadata_table_to_db_entry.
     """
@@ -54,23 +56,24 @@ def chemstation_data_to_db(data_lib_path: str,
 
     uv_paths_list = chemstation_methods.uv_filepaths_to_list(data_lib_path)
     uv_paths_list = check_if_chemstation_tables_needs_updating(
-        uv_paths_list,
-        db_filepath,
-        raw_chemstation_metadata_table_name
+        uv_paths_list, db_filepath, raw_chemstation_metadata_table_name
     )
     # if uv_paths_list is empty, skip rest of function.
     if uv_paths_list:
         # get the uv_metadata and data as lists either from the pickle or the process.
         # pickle vars
         pickle_filename = "chemstation_data_dicts_tuple.pk"
-        pickle_filepath = os.path.join(os.getcwd(), pickle_filename)
-        chemstation_data_dicts_tuple = pickle_chemstation_data.chemstation_pickle_interface(
-            pickle_filepath, uv_paths_list, db_filepath
+
+        pickle_filepath = pickle_chemstation_data.pickle_path(pickle_filename)
+        chemstation_data_dicts_tuple = (
+            pickle_chemstation_data.chemstation_pickle_interface(
+                pickle_filepath, uv_paths_list, db_filepath
+            )
         )
 
         assert isinstance(
             chemstation_data_dicts_tuple, tuple
-        ), "the data_dicts var is expected to be of type tuple."
+        ), f"the data_dicts var is expected to be of type tuple, but {type(chemstation_data_dicts_tuple)}"
 
         chemstation_metadata_list, chromatogram_spectrum_list = (
             chemstation_data_dicts_tuple[0],
@@ -101,7 +104,9 @@ def chemstation_data_to_db(data_lib_path: str,
         )
     elif not uv_paths_list:
         print(f"{len(uv_paths_list)} new uv_files to add to db.\n")
-        print(f"Therefore, skipping remaining raw chemstation process. {os.path.abspath(__file__)}\n")
+        print(
+            f"Therefore, skipping remaining raw chemstation process. {os.path.abspath(__file__)}\n"
+        )
         return None
     pickle_cleanup(pickle_filepath)
     print("###\n\nEND CHEMSTATION RAW DATA PROCESSING\n\n###\n")
@@ -120,7 +125,7 @@ def check_if_chemstation_tables_needs_updating(
     Check if the path column of the chemstation db table corresponds to the uv_paths_list. if any in uv_paths_list not in db, tell user.
     """
     print(f"checking if any files in uv_paths_list not in {db_table_name}..\n")
-    
+
     query = f"""
     SELECT
         path
@@ -133,19 +138,20 @@ def check_if_chemstation_tables_needs_updating(
     assert os.path.isfile(db_filepath), "db file not found"
 
     try:
-        assert db_methods.test_db_table_exists(db_filepath, 
-        db_table_name)
+        assert db_methods.test_db_table_exists(db_filepath, db_table_name)
     except:
-        print('table not found, continuing..\n')
+        print("table not found, continuing..\n")
         return uv_paths_list
 
     # try to connect to the db table provided and extract the paths column as a pd series
     try:
-        print(f'connecting to {db_filepath}..\n')
-        
+        print(f"connecting to {db_filepath}..\n")
+
         with db.connect(db_filepath) as con:
             db_path_series = con.sql(query).df()["path"]
-            print(f"in {db_table_name}, {len(uv_paths_series)} rows of \"path\" found..\n")
+            print(
+                f'in {db_table_name}, {len(uv_paths_series)} rows of "path" found..\n'
+            )
 
     except db.CatalogException as e:
         print(e)
@@ -153,20 +159,20 @@ def check_if_chemstation_tables_needs_updating(
             con.sql("SELECT name FROM sqlite_master WHERE type='table'").show()
         return uv_paths_list
 
-    uv_paths_list = uv_paths_series[
-        ~uv_paths_series.isin(db_path_series)
-    ].to_list()
+    uv_paths_list = uv_paths_series[~uv_paths_series.isin(db_path_series)].to_list()
 
-    print(f"{uv_paths_list} not found in db \"path\" column..\n")
-    
+    print(f'{uv_paths_list} not found in db "path" column..\n')
+
     try:
         assert uv_paths_list
-        
+
     except AssertionError as e:
         print(e)
         return uv_paths_list
     else:
-        print(f"The following files are in the specified filepath but are not in {db_table_name}:\n")
+        print(
+            f"The following files are in the specified filepath but are not in {db_table_name}:\n"
+        )
         print(uv_paths_list)
     finally:
         return uv_paths_list
