ec9ff53dbc16ec23a4ce853eecdbba6ed9fac954
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-05-17 14:36:38 +1000

    formatting
    Former-commit-id: c5c850e2559001db046683846ce2bb4f2ccaaf32

--- wine_analysis_hplc_uv/core/super_table_pipe/super_table_pipe.py
+++ wine_analysis_hplc_uv/core/super_table_pipe/super_table_pipe.py
@@ -4,14 +4,17 @@ from . import (
     selected_avantor_runs,
 )
 from ...devtools import function_timer as ft, project_settings
+from ...ux_methods import ux_methods as ux
 
 
 @ft.timeit
 def super_table_pipe(chemstation_df, sample_tracker_df, cellartracker_df, how_chemstation_sampletracker_join: str):
     print("###\n\nSUPER TABLE PIPE\n\n###\n")
+    
     assert not chemstation_df.empty, "chemstation_df is empty\n"
     assert not sample_tracker_df.empty, "sample_tracker_df is empty\n"
     assert not cellartracker_df.empty, "cellartracker_df is empty\n50"
+
     df = (
         chemstation_df.pipe(selected_avantor_runs.selected_avantor_runs)
         .pipe(chemstation_sample_tracker_join.chemstation_sample_tracker_join, sample_tracker_df, how = how_chemstation_sampletracker_join)
