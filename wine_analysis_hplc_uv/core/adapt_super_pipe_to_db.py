2066132913dc4047bbf448ac400f220c7b2b2f6d
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-05-17 14:34:37 +1000

    makes the first component of the load_super_table function into a sub function with user prompt, and assert statemetns to make sure the retrieved df's are not empty. Removes a line forcing cellartracker vintage column to be int, and a line to check if any of the values in the column are missing. I guess to deal with issues of mixed data types. hAve removed for now, see if thats an issue later..
    Former-commit-id: b4e9b8afe0b86282a0f37c90bac1564b3861c488
    Former-commit-id: c8ca5365d98bb9c0ea07c08401dcb1e2903b8c52
    Former-commit-id: 87c6d56404d375a219ab0fd66d6f60f92eea40c9

--- wine_analysis_hplc_uv/core/adapt_super_pipe_to_db.py
+++ wine_analysis_hplc_uv/core/adapt_super_pipe_to_db.py
@@ -6,35 +6,31 @@ from ..core.super_table_pipe import super_table_pipe
 from ..db_methods import db_methods
 from ..devtools import function_timer as ft
 from ..devtools import project_settings
+from ..ux_methods import ux_methods as ux
 
 
 @ft.timeit
 def load_super_table(db_filepath: str, table_1, table_2, table_3, tbl_name):
     print("###\n\nLoad Super Table\n\n###\n\n")
 
-    with db.connect(db_filepath) as con:
-        con.sql("drop table if exists super_table")
-
-        print(f"loading {table_1} into df..\n")
-        chemstation_df = con.sql(f"SELECT * FROM {table_1}").df()
-        print(f"loading {table_2} into df..\n")
-        sample_tracker_df = con.sql(f"SELECT * FROM {table_2}").df()
-        print(f"loading {table_3} into df..\n")
-        cellartracker_df = con.sql(f"SELECT * FROM {table_3}").df()
-
-    cellartracker_df.vintage = cellartracker_df.vintage.astype("Int64")
-
-    print(cellartracker_df[cellartracker_df["vintage"] == "empty"])
-
-    print(f"Starting with {chemstation_df.shape[0]} rows in chemstation_df..\n")
-    how_chemstation_sampletracker_join = "left"
-    super_table_df = super_table_pipe.super_table_pipe(
-        chemstation_df,
-        sample_tracker_df,
-        cellartracker_df,
-        how_chemstation_sampletracker_join=how_chemstation_sampletracker_join,
+    ch_df, st_df, ct_df = ux.ask_user_and_execute(
+        f"fetch {table_1}, {table_2}, and {table_3} tables as dfs from {db_filepath}?",
+        fetch_tables,
+        db_filepath=db_filepath,
+        tbl_1=table_1,
+        tbl_2=table_2,
+        tbl_3=table_3,
     )
+    assert not ch_df.empty
+    assert not st_df.empty
+    assert not ct_df.empty
 
+    # with db.connect(db_filepath) as con:
+    #     write_super_table_to_db(super_table_df, db_filepath, tbl_name)
+    return None
+
+
+def fetch_tables(db_filepath: str, tbl_1: str, tbl_2: str, tbl_3: str) -> tuple:
     with db.connect(db_filepath) as con:
         write_super_table_to_db(super_table_df, db_filepath, tbl_name)
     return None
