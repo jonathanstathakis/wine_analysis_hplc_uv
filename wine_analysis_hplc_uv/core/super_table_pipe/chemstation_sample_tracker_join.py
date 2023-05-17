4856297a609d52c5b4b0c287bfe90b304059e83b
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-05-17 23:31:58 +1000

    made chemsation_samplee_tracker_join into 'join_dfs' a sub-function of the new chemstation_sample_tracker_join function, primarily to enable better interafce between df.pipe and ux_methods. Modifies ux dialog thoughout the process for better interaction and  debugging
    Former-commit-id: 6249ccc11737744d7e54028f4c34b3bec2f9e2ad
    Former-commit-id: e84ac00990c8dc5a7da8a9bfba732a6eb0347863
    Former-commit-id: ae8caf3155963996fa421551e2528647c5c72faa

--- wine_analysis_hplc_uv/core/super_table_pipe/chemstation_sample_tracker_join.py
+++ wine_analysis_hplc_uv/core/super_table_pipe/chemstation_sample_tracker_join.py
@@ -2,36 +2,51 @@
 """
 import pandas as pd
 
-from ...devtools import function_timer as ft, project_settings
+from ...devtools import function_timer as ft
+from ...devtools import project_settings
+from ...ux_methods import ux_methods as ux
 
 
-def chemstation_sample_tracker_join(
-    in_df: pd.DataFrame, sample_tracker_df: pd.DataFrame, how: str) -> pd.DataFrame:
-    print("\n####\n\njoining metadata table with sample_tracker\n\n####\n")
+def chemstation_sample_tracker_join(in_df: pd.DataFrame, st_df: pd.DataFrame, how: str):
+    def join_dfs(in_df: pd.DataFrame, st_df: pd.DataFrame, how: str) -> pd.DataFrame:
+        print("joining metadata table with sample_tracker..\n")
 
-    assert not in_df.empty, "in_df is empty"
-    assert not sample_tracker_df.empty, "in_df is empty"
+        assert not in_df.empty, "in_df is empty"
+        assert not st_df.empty, "in_df is empty"
 
-    # drop "new_id" entries with characters..
+        # drop "new_id" entries with characters..
 
-    assert 'new_id' in in_df.columns, "Column 'new_id' does not exist in in_df"
-    assert 'id' in sample_tracker_df.columns, "Column 'id' does not exist in sample_tracker_df"
+        assert "new_id" in in_df.columns, "Column 'new_id' does not exist in in_df"
+        assert "id" in st_df.columns, "Column 'id' does not exist in sample_tracker_df"
 
-    print(f"joining chemstation_metadata, sample_tracker with a {how} join..\n")
-    
-    merge_df = pd.merge(
-        in_df, sample_tracker_df, left_on="new_id", right_on="id", how=how, validate="m:1"
+        print(f"joining chemstation_metadata, sample_tracker with a {how} join..\n")
+
+        merge_df = pd.merge(
+            in_df, st_df, left_on="new_id", right_on="id", how=how, validate="m:1"
+        )
+
+        print(st_df["id"])
+        print(in_df["new_id"])
+
+        print(
+            "\ndf of dims",
+            merge_df.shape,
+            "formed after merge of chemstation_metadata and sample_tracker.\n If this df has more rows than the inital left_df, it is because there were duplicate matches, so the rows were duplicated.\n",
+        )
+        assert (
+            not merge_df.empty
+        ), f"the result of merging in_df and st_df is an empty df. {__name__}"
+
+        return merge_df
+
+    merge_df = ux.ask_user_and_execute(
+        "I will now attempt to join the ch and st dfs. Proceed?",
+        join_dfs,
+        in_df,
+        st_df,
+        how,
     )
 
-    print(sample_tracker_df['id'])
-    print(in_df['new_id'])
-
-    print(
-        "\ndf of dims",
-        merge_df.shape,
-        "formed after merge of chemstation_metadata and sample_tracker.\n If this df has more rows than the inital left_df, it is because there were duplicate matches, so the rows were duplicated.\n",
-    )
-    assert not merge_df.empty, 'At end of merge_df is empty'
     return merge_df
 
 
