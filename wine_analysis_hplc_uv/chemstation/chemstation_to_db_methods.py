6f4e304373dd9efa9fff31a223b734e3f6782d0f
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-05-23 11:21:53 +1000

    chemstation_to_db_methods: adds type checking, context management for db connections, formatting
    Former-commit-id: 458b5bc5995c4c7e7a84bbb39b631e2eb5401d95
    Former-commit-id: b7969216d2389b6b6685b8be5ca18f54fadbecc5

--- wine_analysis_hplc_uv/chemstation/chemstation_to_db_methods.py
+++ wine_analysis_hplc_uv/chemstation/chemstation_to_db_methods.py
@@ -1,5 +1,5 @@
 """
-A module to contain chemstation database interface methods
+A module to dbfilepathtain chemstation database interface methods
 """
 import json
 import os
@@ -9,13 +9,14 @@ import duckdb as db
 import pandas as pd
 from ..db_methods import db_methods
 from ..devtools import function_timer as ft
+from typing import Tuple
 
 
 @ft.timeit
-def write_chemstation_data_to_db_entry(chemstation_data_dicts_tuple, con):
+def write_chemstation_data_to_db_entry(chemstation_data_dicts_tuple: Tuple[dict, dict], dbfilepath: str) -> None:
     # get the intended table name
-    chromatogram_spectrum_table_name = "chromatogram_spectra"
-    chemstation_metadata_table_name = "chemstation_metadata"
+    chromatogram_spectrum_tblname = "chromatogram_spectra"
+    chemstation_metadata_tblname = "chemstation_metadata"
 
     # extract the lists from the list object
     chemstation_metadata_list, chromatogram_spectrum_list = (
@@ -28,10 +29,10 @@ def write_chemstation_data_to_db_entry(chemstation_data_dicts_tuple, con):
     ):
         write_chemstation_to_db(
             chemstation_metadata_list,
-            chemstation_metadata_table_name,
+            chemstation_metadata_tblname,
             chromatogram_spectrum_list,
-            chromatogram_spectrum_table_name,
-            con,
+            chromatogram_spectrum_tblname,
+            dbfilepath,
         )
     else:
         print(
@@ -42,68 +43,70 @@ def write_chemstation_data_to_db_entry(chemstation_data_dicts_tuple, con):
 
 
 def write_chemstation_to_db(
-    chemstation_metadata_list,
-    chemstation_metadata_table_name,
-    chromatogram_spectrum_list,
-    chromatogram_spectrum_table_name,
-    con,
+    chemstation_metadata_list: list,
+    chemstation_metadata_tblname : str,
+    chromatogram_spectrum_list: list,
+    chromatogram_spectrum_tblname: str,
+    dbfilepath: str,
 ):
     # write metadata table, chromatogram_spectra table.
     chemstation_metadata_to_db(
-        chemstation_metadata_list, chemstation_metadata_table_name, con
+        chemstation_metadata_list, chemstation_metadata_tblname, dbfilepath
     )
     chromatogram_spectra_to_db(
-        chromatogram_spectrum_list, chromatogram_spectrum_table_name, con
+        chromatogram_spectrum_list, chromatogram_spectrum_tblname, dbfilepath
     )
     return None
 
 
-def chemstation_metadata_to_db(chemstation_metadata_list, table_name, con):
+def chemstation_metadata_to_db(chemstation_metadata_list: list, tblname: str, dbfilepath: str):
     df = metadata_list_to_df(chemstation_metadata_list)
-    df.pipe(check_if_table_exists_write_df_to_db, table_name, con)
+    df.pipe(check_if_table_exists_write_df_to_db, tblname, dbfilepath)
+    return None
 
 
-def chromatogram_spectra_to_db(chromatogram_spectrum_list, table_name, con):
-    df = uv_data_list_to_df(chromatogram_spectrum_list, con)
-    df.pipe(check_if_table_exists_write_df_to_db, table_name, con)
+def chromatogram_spectra_to_db(chromatogram_spectrum_list: list, tblname: str, dbfilepath: str)->None:
+    df = uv_data_list_to_df(chromatogram_spectrum_list, dbfilepath)
+    df.pipe(check_if_table_exists_write_df_to_db, tblname, dbfilepath)
+    return None
 
 
-def check_if_table_exists_write_df_to_db(df, table_name, con):
+def check_if_table_exists_write_df_to_db(df: pd.DataFrame, tblname: str, dbfilepath)-> None:
     """check if table currently exists:
     if does, prompt whether overwrite.
         if yes:
             drop table
             write table
         if no:
-            continue.
+            dbfilepathtinue.
     """
     # if yes, ask whether to overwrite. if not, ask to write.
-    if table_in_db_query(table_name, con):
-        if input(f"table {table_name} in {db}. overwrite? (y/n):") == "y":
-            con.sql(f"DROP TABLE IF EXISTS {table_name}").execute()
-            write_df_to_db(df, table_name, con)
+    if table_in_db_query(tblname, dbfilepath):
+        if input(f"table {tblname} in {db}. overwrite? (y/n):") == "y":
+            dbfilepath.sql(f"DROP TABLE IF EXISTS {tblname}").execute()
+            write_df_to_db(df, tblname, dbfilepath)
         else:
-            print(f"leaving {table_name} as is.")
+            print(f"leaving {tblname} as is.")
 
     else:
-        if input(f"table {table_name} not found, write to db? (y/n)") == "y":
-            write_df_to_db(df, table_name, con)
+        if input(f"table {tblname} not found, write to db? (y/n)") == "y":
+            write_df_to_db(df, tblname, dbfilepath)
         else:
-            # continue without writing.
+            # dbfilepathtinue without writing.
             print("test")
 
 
-def table_in_db_query(table_name, con):
-    query = f"SELECT COUNT(*) FROM information_schema.tables WHERE table_name = '{table_name}'"
-    result = con.sql(query).fetchone()
+def table_in_db_query(tblname, dbfilepath):
+    query = f"SELECT COUNT(*) FROM information_schema.tables WHERE tblname = '{tblname}'"
+    result = dbfilepath.sql(query).fetchone()
     return result[0] > 0
 
 
-def metadata_list_to_df(uv_metadata_list):
+def metadata_list_to_df(uv_metadata_list: list)-> pd.DataFrame:
     return pd.json_normalize(data=uv_metadata_list)
 
 
-def uv_data_list_to_df(uv_data_list: list, con: db.DuckDBPyConnection) -> None:
+def uv_data_list_to_df(uv_data_list: list, dbfilepath: str) -> None:
     """ """
     spectrum_dfs = []
     for uv_data in uv_data_list:
@@ -111,18 +114,18 @@ def uv_data_list_to_df(uv_data_list: list, con: db.DuckDBPyConnection) -> None:
         data["hash_key"] = uv_data["hash_key"]
         spectrum_dfs.append(data)
 
-    combined_df = pd.concat(spectrum_dfs)
+    combined_df = pd.dbfilepathcat(spectrum_dfs)
     return combined_df
 
 
-def write_df_to_db(df, table_name, con):
+def write_df_to_db(df: pd.DataFrame, tblname: str, dbfilepath: str):
     try:
-        print(f"creating {table_name} table from df")
-        con.execute(f"CREATE TABLE {table_name} AS SElECT * FROM df")
+        print(f"creating {tblname} table from df")
+        dbfilepath.execute(f"CREATE TABLE {tblname} AS SElECT * FROM df")
     except Exception as e:
         print(e)
 
-    db_methods.display_table_info(con, table_name)
+    db_methods.display_table_info(dbfilepath, tblname)
     return None
 
 
