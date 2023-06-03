23ccaff72fa10be902a42da992fdbb4775154ee9
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-03 17:28:51 +1000

    added basic tests of stracker initialization

--- tests/test_sampletracker.py
+++ tests/test_sampletracker.py
@@ -7,12 +7,6 @@ import pandas as pd
 from wine_analysis_hplc_uv.sampletracker import sampletrackerprocesser as stracker
 
 
-def test_sampletracker_init():
-    assert stracker.SampleTracker(get_columns_dict(), get_google_api_dict())
-
-    return None
-
-
 def get_columns_dict() -> dict:
     # column names as keys, dtypes as values
     return {
@@ -35,6 +29,25 @@ def get_google_api_dict() -> dict:
         range="sample_tracker!A1:Z200",
         creds_parent_path=os.environ.get("GOOGLE_SHEETS_API_CREDS_PARENT_PATH"),
     )
+
+
+def strack():
+    return stracker.SampleTracker(get_columns_dict(), get_google_api_dict())
+
+
+def test_sampletracker_init(strack: stracker.SampleTracker = strack()):
+    assert strack
+
+    return None
+
+
+def test_strack_df_is_pd(strack: stracker.SampleTracker = strack()) -> None:
+    assert isinstance(strack.df, pd.DataFrame)
+    return None
+
+
+def test_strack_df_not_empty(strack: stracker.SampleTracker = strack()) -> None:
+    assert not strack.df.empty
 
 
 def main():
