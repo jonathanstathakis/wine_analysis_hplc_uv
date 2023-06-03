44ef787aa841b3bb890433ed94d3610d636939af
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-03 17:29:29 +1000

    modified sampletracker df initialization to rely on an externally initialized set of dicts for the columns and the google api information

--- src/wine_analysis_hplc_uv/sampletracker/sampletrackerprocesser.py
+++ src/wine_analysis_hplc_uv/sampletracker/sampletrackerprocesser.py
@@ -2,7 +2,7 @@
 Driver class for sampletracker processes
 """
 
-from wine_analysis_hplc_uv.sampletracker import sample_tracker_methods as st
+from wine_analysis_hplc_uv.sampletracker import sample_tracker_methods as st_methods
 from wine_analysis_hplc_uv.sampletracker import sample_tracker_cleaner
 from wine_analysis_hplc_uv.sampletracker import init_raw_sample_tracker_table
 import pandas as pd
@@ -13,25 +13,20 @@ class SampleTracker:
     Class to wrap around all pertinant sampletracker methods
     """
 
-    def __init__(self) -> None:
-        self.df: pd.DataFrame = self.st_df_helper()
+    def __init__(self, columns_dict: dict, google_api_dict: dict) -> None:
+        self.df: pd.DataFrame = self.st_df_helper(columns_dict, google_api_dict)
         self.clean_df: pd.DataFrame = self.clean_st()
         self.tbl_name = "sampletracker"
 
-    def st_to_db(self, clean: bool) -> None:
-        df: pd.DataFrame = self.st_df_helper()
-        if clean:
-            self.clean_df.pipe(self.st_to_db)
-        else:
-            self.df.pipe(self.st_to_db, self.db_filepath)
-
-    def st_df_helper(self) -> pd.DataFrame:
+    def st_df_helper(self, columns_dict: dict, google_api_dict: dict) -> pd.DataFrame:
         """_summary_
         Build the sampletracker df from the Google Sheets table.
         Returns:
             pd.DataFrame: _description_
         """
-        df: pd.DataFrame = st.sample_tracker_df_builder()
+        df: pd.DataFrame = st_methods.sample_tracker_df_builder(
+            columns_dict, google_api_dict
+        )
         return df
 
     def st_to_db_helper(
@@ -44,3 +39,10 @@ class SampleTracker:
     def clean_st(self) -> pd.DataFrame:
         df: pd.DataFrame = sample_tracker_cleaner.sample_tracker_df_cleaner(self.df)
         return df
+
+    def st_to_db(self, clean: bool) -> None:
+        df: pd.DataFrame = self.st_df_helper()
+        if clean:
+            self.clean_df.pipe(self.st_to_db)
+        else:
+            self.df.pipe(self.st_to_db, self.db_filepath)
