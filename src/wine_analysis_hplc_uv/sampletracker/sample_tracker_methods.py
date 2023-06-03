58c8f654a3e87a0960e21b8e2c3fac458e650ceb
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-03 17:29:29 +1000

    modified sampletracker df initialization to rely on an externally initialized set of dicts for the columns and the google api information
    Former-commit-id: 44ef787aa841b3bb890433ed94d3610d636939af
    Former-commit-id: 649f970cbe189c678bc24f4682454ca2b431437c

--- src/wine_analysis_hplc_uv/sampletracker/sample_tracker_methods.py
+++ src/wine_analysis_hplc_uv/sampletracker/sample_tracker_methods.py
@@ -1,47 +1,21 @@
 import os
-import sys
+from typing import Dict
 import pandas as pd
 from wine_analysis_hplc_uv.google_sheets_api import google_sheets_api
-from wine_analysis_hplc_uv.df_methods import df_cleaning_methods
 
 
-def sample_tracker_df_builder():
+def sample_tracker_df_builder(columns_dict: Dict, google_api_dict: Dict):
     df = google_sheets_api.get_sheets_values_as_df(
-        spreadsheet_id="15S2wm8t6ol2MRwTzgKTjlTcUgaStNlA22wJmFYhcwAY",
-        range="sample_tracker!A1:Z200",
-        creds_parent_path=os.environ.get("GOOGLE_SHEETS_API_CREDS_PARENT_PATH"),
+        spreadsheet_id=google_api_dict["spreadsheet_id"],
+        range=google_api_dict["range"],
+        creds_parent_path=google_api_dict["creds_parent_path"],
     )
-    print(df.columns)
-    assert "notes" in df.columns
 
-    df = df[
-        [
-            "detection",
-            "sampler",
-            "id",
-            "vintage",
-            "name",
-            "open_date",
-            "sampled_date",
-            "added_to_cellartracker",
-            "notes",
-            "size",
-        ]
-    ]
-    df = df.astype(
-        {
-            "sampler": pd.StringDtype(),
-            "detection": pd.StringDtype(),
-            "id": pd.StringDtype(),
-            "vintage": pd.StringDtype(),
-            "name": pd.StringDtype(),
-            "open_date": pd.StringDtype(),
-            "sampled_date": pd.StringDtype()    ,
-            "added_to_cellartracker": pd.StringDtype(),
-            "notes": pd.StringDtype(),
-            "size": pd.StringDtype(),
-        }
-    )
+    # from the imported range, only select the specified columns
+    df = df[columns_dict.keys()]
+
+    # set the dtype of those columns
+    df = df.astype(columns_dict)
 
     return df
 
