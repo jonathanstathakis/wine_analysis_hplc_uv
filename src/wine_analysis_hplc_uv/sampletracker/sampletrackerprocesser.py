3b023035e1a3a4630b8c5e16f140d8d173cf4e38
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-03 20:02:24 +1000

    add docstring to SampleTracker, update clean_st to clean_df_helper(). add stracker clean_df tests

--- src/wine_analysis_hplc_uv/sampletracker/sampletrackerprocesser.py
+++ src/wine_analysis_hplc_uv/sampletracker/sampletrackerprocesser.py
@@ -1,5 +1,9 @@
 """
 Driver class for sampletracker processes
+
+2023-06-03 17:29:39
+TODO:
+- [ ] continue organizing and cleaning SampleTracker class
 """
 
 from wine_analysis_hplc_uv.sampletracker import sample_tracker_methods as st_methods
@@ -9,13 +13,9 @@ import pandas as pd
 
 
 class SampleTracker:
-    """
-    Class to wrap around all pertinant sampletracker methods
-    """
-
     def __init__(self, columns_dict: dict, google_api_dict: dict) -> None:
         self.df: pd.DataFrame = self.st_df_helper(columns_dict, google_api_dict)
-        self.clean_df: pd.DataFrame = self.clean_st()
+        self.clean_df: pd.DataFrame = self.clean_df_helper()
         self.tbl_name = "sampletracker"
 
     def st_df_helper(self, columns_dict: dict, google_api_dict: dict) -> pd.DataFrame:
@@ -29,6 +29,12 @@ class SampleTracker:
         )
         return df
 
+    def clean_df_helper(self) -> pd.DataFrame:
+        clean_df: pd.DataFrame = sample_tracker_cleaner.sample_tracker_df_cleaner(
+            self.df
+        )
+        return clean_df
+
     def st_to_db_helper(
         self, df: pd.DataFrame, db_filepath: str, db_tbl_name: str
     ) -> None:
@@ -36,13 +42,11 @@ class SampleTracker:
             df=df, db_filepath=db_filepath, db_table_name=db_tbl_name
         )
 
-    def clean_st(self) -> pd.DataFrame:
-        df: pd.DataFrame = sample_tracker_cleaner.sample_tracker_df_cleaner(self.df)
-        return df
-
-    def st_to_db(self, clean: bool) -> None:
-        df: pd.DataFrame = self.st_df_helper()
+    def st_to_db(self, clean: bool, db_filepath: str) -> None:
         if clean:
             self.clean_df.pipe(self.st_to_db)
         else:
-            self.df.pipe(self.st_to_db, self.db_filepath)
+            self.df.pipe(self.st_to_db, db_filepath)
+        return None
+
+    # def to_sheets(self)-> None:
