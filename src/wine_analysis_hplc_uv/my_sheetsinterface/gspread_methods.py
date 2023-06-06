cb36f22bb38388853e4523cc74ebca6ef8e2480b
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-06 12:35:36 +1000

    add class MySheet to wrap gspread
    Former-commit-id: 54b2182f5f265d3449cc9299c9bb7681a9aeadd7
    Former-commit-id: c247cbdd1735fd7359be17c75a8e32169986f701

--- src/wine_analysis_hplc_uv/my_sheetsinterface/gspread_methods.py
+++ src/wine_analysis_hplc_uv/my_sheetsinterface/gspread_methods.py
@@ -2,8 +2,25 @@ import gspread
 import os
 
 
+class MySheet:
+    def __init__(self, key, sheet_title):
+        gc = get_service_account()
+        sh = gc.open_by_key(key)
+        wksh = sh.worksheet(sheet_title)
+
+
 def get_service_account():
     return gspread.service_account()
+
+
+def open_worksheet(self, sheet_title: str):
+    try:
+        wksh = self.sh.worksheet(sheet_title)
+    except gspread.exceptions.WorksheetNotFound:
+        print(f"Worksheet '{sheet_title}' not found. Creating a new one...")
+        wksh = self.sh.add_worksheet(title=sheet_title, rows="100", cols="20")
+        print(f"Worksheet '{sheet_title}' created.")
+    return wksh
 
 
 def get_test_st_sh(service_account):
@@ -15,3 +32,21 @@ def get_test_st_sh(service_account):
 def get_sheet_list(sample_tracker_sh):
     sheet_list = sample_tracker_sh.worksheets()
     return sheet_list
+
+
+# def wksh_to_df(
+#     gc,
+# ):
+#     gc = gspread_methods.get_service_account()
+
+#     sh = gc.open_by_key("15S2wm8t6ol2MRwTzgKTjlTcUgaStNlA22wJmFYhcwAY")
+
+#     sample_tracker_wksh = sh.worksheet(sheet_title)
+
+#     values = sample_tracker_wksh.get_all_values()
+#     columns = values[0]
+#     data = values[1:]
+
+#     df = pd.DataFrame(data=data, columns=columns)
+
+#     return df
