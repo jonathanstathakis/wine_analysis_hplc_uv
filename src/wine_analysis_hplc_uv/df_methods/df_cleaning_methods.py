3a71bcb264429ebe8996269bec5aa85794c82413
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-07 09:25:52 +1000

    updated df_cleaning_methods.df_string_cleaner and sample_tracker_cleaner to apply string cleaning methods to index and column within the df_cleaner func.
    Former-commit-id: 0faddc39676e31130d8ac8569b1b728213591588
    Former-commit-id: 8dd8274295e0cf3285461fef1b3f7d2d50b6762a

--- src/wine_analysis_hplc_uv/df_methods/df_cleaning_methods.py
+++ src/wine_analysis_hplc_uv/df_methods/df_cleaning_methods.py
@@ -5,12 +5,17 @@ import pandas as pd
 
 
 def df_string_cleaner(df: pd.DataFrame) -> pd.DataFrame:
+    # stripping and lowering case of all string values in df
     def lowercase_and_strip(value):
         if isinstance(value, str):
             return value.lower().strip()
         return value
 
-    print("lowering case and stripping all str columns in df")
     df = df.applymap(lowercase_and_strip)
+
+    # stripping and lowering case of all column and index values in df
+
+    df = df.columns.astype(str).str.strip().str.lower()
+    df = df.index.astype(str).str.strip().str.lower()
 
     return df
