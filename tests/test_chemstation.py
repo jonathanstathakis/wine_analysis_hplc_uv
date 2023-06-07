1b78124dc0a0727996d7f8a1340271ed8fe09d69
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-07 23:19:49 +1000

    basis of chemstation test module
    Former-commit-id: a30151772d030fd75d9c41944b7f2c7b3898480c
    Former-commit-id: 7e5b4ffc29e4ed0d51a048246cebd16b3c1ec497

---
+++ tests/test_chemstation.py
@@ -0,0 +1,20 @@
+"""
+
+"""
+from .mytestmethods.mytestmethods import test_report
+
+
+def test_chemstation():
+    tests = []
+
+    test_report(tests)
+    return None
+
+
+def main():
+    test_chemstation()
+    return None
+
+
+if __name__ == "__main__":
+    main()
