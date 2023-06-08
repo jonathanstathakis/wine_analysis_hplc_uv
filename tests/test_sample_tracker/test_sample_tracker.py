c7342e7521a62588448787edb475d25fe291b71f
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-08 16:13:32 +1000

    removed superfluous 'df_builder' method, df initialized directly from wksh object now.

--- tests/test_sample_tracker/test_sample_tracker.py
+++ tests/test_sample_tracker/test_sample_tracker.py
@@ -20,15 +20,17 @@ from wine_analysis_hplc_uv.sampletracker import sample_tracker_processor
 from tests.mytestmethods.mytestmethods import test_report
 from wine_analysis_hplc_uv.df_methods import df_methods
 from wine_analysis_hplc_uv.my_sheetsinterface.gspread_methods import WorkSheet
+import pandas as pd
 
 
 def test_sample_tracker():
     tests = [
-        (test_sample_tracker_df_builder,),
+        # (test_sample_tracker_df_builder,), # 2023-06-08 16:09:14 deleted this method, instead initializing directly from the wksh member obj.
         (test_sample_tracker_class_init,),
         (test_sample_tracker_df,),
         (test_sample_tracker_clean_df,),
         (test_to_sheets,),
+        # (test_to_db, get_SampleTracker(get_key())),
     ]
 
     test_report(tests)
@@ -82,6 +84,29 @@ def test_to_sheets(key=get_key(), sheet_title_2="test_to_sheets"):
     new_wksh = WorkSheet(key=key, sheet_title=sheet_title_2)
     assert sample_tracker.df.equals(new_wksh.sheet_df)
     new_wksh.delete_sheet(new_wksh.wksh)
+
+
+def get_db_filepath():
+    return os.path.join(os.getcwd(), "test_st.db")
+
+
+def get_db_tbl_name():
+    return "test_st_tbl"
+
+
+def test_to_db(st):
+    db_filepath = get_db_filepath()
+    db_tbl_name = get_db_tbl_name()
+    st.to_db_helper(db_filepath=db_filepath, db_tbl_name=db_tbl_name)
+
+    db_df = None
+    import duckdb as db
+
+    with db.connect(db_filepath) as con:
+        db_df = con.sql(f"SELECT * FROM {db_tbl_name}").df()
+    os.rmtree(db_filepath)  # cleanup
+
+    pd.testing.assert_frame_equal(st.df, db_df)
 
 
 def main():
