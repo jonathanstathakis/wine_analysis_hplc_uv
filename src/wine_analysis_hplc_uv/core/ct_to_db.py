f08f0e3dc54a4b78a89e3104c21c78e052236799
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-09 14:22:31 +1000

    add config functions for class to db actions
    Former-commit-id: cd589d5c8e5a6552ed3bb66fce0d52c87aae31a7
    Former-commit-id: b5baeb0b23d54822183e6f0a6112a0198a0f5bec

---
+++ src/wine_analysis_hplc_uv/core/ct_to_db.py
@@ -0,0 +1,29 @@
+from wine_analysis_hplc_uv.cellartracker_methods.my_cellartracker_class import (
+    MyCellarTracker,
+)
+
+from wine_analysis_hplc_uv.definitions import DB_DIR
+import os
+import duckdb as db
+
+
+def ct_to_db(db_filepath: str, ct_tbl: str, un: str, pw: str):
+    if not os.path.isfile(DB_DIR):
+        con = db.connect(DB_DIR)
+        con.close()
+
+    ct = MyCellarTracker(username=un, password=pw)
+    ct.to_db(db_filepath=db_filepath, tbl_name=ct_tbl)
+
+
+def main():
+    db_filepath = DB_DIR
+    ct_tbl = "cellar_tracker"
+    un = os.environ.get("CELLAR_TRACKER_UN")
+    pw = os.environ.get("CELLAR_TRACKER_PW")
+
+    ct_to_db(db_filepath, ct_tbl, un, pw)
+
+
+if __name__ == "__main__":
+    main()
