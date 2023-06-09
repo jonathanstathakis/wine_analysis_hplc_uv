34bd420f108c268d13edb268859fcc73227e04c6
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-09 14:20:14 +1000

    reduce verbosity of tests

--- tests/chemstation_tests/__init__.py
+++ tests/chemstation_tests/__init__.py
@@ -2,5 +2,5 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 chemstation_logger = logging.getLogger("wine_analysis_hplc_uv.chemstation")
-chemstation_logger.setLevel(logging.INFO)
+chemstation_logger.setLevel(logging.ERROR)
 test_logger = logging.getLogger(__name__)
