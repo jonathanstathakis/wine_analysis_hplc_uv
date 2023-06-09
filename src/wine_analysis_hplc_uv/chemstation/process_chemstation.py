a85fdbd034f33fe04bf9bdcc30a59e8c7ee8e35c
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-09 14:19:45 +1000

    removed superf function
    Former-commit-id: 4445dfc965bc7e681fbe3bb65d5dc3a7f90b0183
    Former-commit-id: b0e40e78880eba492fbc44d358efce503a7a4879

--- src/wine_analysis_hplc_uv/chemstation/process_chemstation.py
+++ src/wine_analysis_hplc_uv/chemstation/process_chemstation.py
@@ -13,34 +13,6 @@ from wine_analysis_hplc_uv.chemstation import ch_data_multiprocess
 from wine_analysis_hplc_uv.chemstation import logger
 
 
-def chemstation(
-    data_lib_path: str, db_filepath: str, ch_metadata_tblname: str, ch_sc_tblname: str
-):
-    """
-    main driver file, handle any preprocessing then activate write_ch_metadata_table_to_db_entry.
-    """
-    # pickle vars
-    pickle_filename = "chemstation_process_picklejar/chemstation_data_dicts_tuple.pk"
-    pickle_filepath = os.path.join(data_lib_path, pickle_filename)
-
-    # get the .D paths
-    uv_paths_list: list[str] = chemstation_methods.uv_filepaths_to_list(
-        root_dir_path=data_lib_path
-    )
-
-    # get the uv_metadata and data as lists either from the pickle or the process
-    ch_data: Tuple[List[dict], List[dict]] = pickle_chemstation_data.pickle_interface(
-        pickle_filepath, uv_paths_list
-    )
-
-    # write the uv_metadata and data to tables in the given db.
-    chemstation_to_db_methods.write_chemstation_to_db(
-        ch_data, db_filepath, ch_metadata_tblname, ch_sc_tblname
-    )
-
-    return None
-
-
 def process_chemstation_uv_files(
     uv_paths_list: List[str],
 ) -> Tuple[List[dict], List[dict]]:
