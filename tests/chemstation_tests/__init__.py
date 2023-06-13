1637fb150781fec47e3655bc211082b55cca95ee
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-13 14:42:13 +1000

    reduce verbosity of logger
    Former-commit-id: 5aec2dc13d06090902f44470c3562df9598ec23e
    Former-commit-id: 2b52b6d6e062ef2512c71b33eab5952c55f47027

--- tests/chemstation_tests/__init__.py
+++ tests/chemstation_tests/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 chemstation_logger = logging.getLogger("wine_analysis_hplc_uv.chemstation")
 chemstation_logger.setLevel(logging.DEBUG)
 test_logger = logging.getLogger(__name__)
