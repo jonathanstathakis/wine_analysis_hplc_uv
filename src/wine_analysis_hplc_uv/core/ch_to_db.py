99fba1a29fe8bb58a7567fa7d1025065db321bd1
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-13 15:08:28 +1000

    extract chemstation to db lines to ch_to_db
    Former-commit-id: cd34ecadefdf76f5fc97d207120f33186417bd8b
    Former-commit-id: f12f5bfedd5b19633711a4ce0a25df5524fdaf16

---
+++ src/wine_analysis_hplc_uv/core/ch_to_db.py
@@ -0,0 +1,26 @@
+from wine_analysis_hplc_uv.definitions import DB_DIR, LIB_DIR
+
+from wine_analysis_hplc_uv.chemstation import (
+    ch_metadata_tbl_cleaner,
+    chemstationprocessor,
+)
+
+# chemstation
+
+
+def ch_to_db(lib_path: str, mtadata_tbl: str, sc_tbl: str, db_path: str):
+    ch = chemstationprocessor.ChemstationProcessor(lib_path=lib_path)
+    ch.to_db(db_filepath=DB_DIR, ch_metadata_dbl_name=mtadata_tbl, ch_sc_tblname=sc_tbl)
+    return None
+
+
+def main():
+    lib_path = LIB_DIR
+    db_path = DB_DIR
+    metadata_tbl = "chemstation_metadata"
+    sc_tbl = "sample_tracker"
+    ch_to_db(lib_path, mtadata_tbl=metadata_tbl, sc_tbl=sc_tbl, db_path=db_path)
+
+
+if __name__ == "__main__":
+    main()
