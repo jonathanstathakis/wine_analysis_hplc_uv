7a5a30ec9068deaa226beb3e3b9320badea72eb3
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-15 00:59:45 +1000

    fix db path typo
    Former-commit-id: 2f3840aface635decbec05aac616c33af4649e19
    Former-commit-id: 62ff22e99a184347fd0885a2f6981f5b486aae83

--- src/wine_analysis_hplc_uv/definitions.py
+++ src/wine_analysis_hplc_uv/definitions.py
@@ -2,7 +2,7 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-DB_PATH = os.environ.get("DB_PATH_PATH")
+DB_PATH = os.environ.get("DB_DIR_PATH")
 
 LIB_DIR = "/Users/jonathan/0_jono_data/mres_data_library/"
 
