d53e1b5e0d5d42addb9da65339faed850a9e64f7
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-14 13:52:03 +1000

    add db tbl names to definitions
    Former-commit-id: af04a8f9ac083e838788685c4d2f1ba5a724cd84
    Former-commit-id: 406bd585643f45a020ebc17d0354f8be73aa3aca

--- src/wine_analysis_hplc_uv/definitions.py
+++ src/wine_analysis_hplc_uv/definitions.py
@@ -5,3 +5,15 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_DIR = os.environ.get("DB_DIR_PATH")
 
 LIB_DIR = "/Users/jonathan/0_jono_data/mres_data_library/"
+
+CH_META_TBL_NAME = "chemstation_metadata"
+CH_DATA_TBL_NAME = "chromatogram_spectra"
+ST_TBL_NAME = "sample_tracker"
+CT_TBL_NAME = "cellar_tracker"
+
+CLEAN_CH_META_TBL_NAME = "c_" + CH_META_TBL_NAME
+# CH_DATA_TBL_NAME = "c_" + CH_DATA_TBL_NAME
+CLEAN_ST_TBL_NAME = "c_" + ST_TBL_NAME
+CLEAN_CT_TBL_NAME = "c_" + CT_TBL_NAME
+
+TEST_DB_PATH = os.path.join(os.getcwd(), "tests", "test.db")
