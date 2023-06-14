c82e06f6ae5f35bfa0831c6c0745bf42a5d81c9f
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-14 12:29:24 +1000

    adjust df_cleaner to only convert idx/col to object dtype if not numeric data detected.
    Former-commit-id: 3130cdd9a23ccf3485fb3c11acf1647900663c87
    Former-commit-id: 1215141b4bd3bfd4f85f111d980edad309f86b7d

--- src/wine_analysis_hplc_uv/df_methods/df_cleaning_methods.py
+++ src/wine_analysis_hplc_uv/df_methods/df_cleaning_methods.py
@@ -15,9 +15,17 @@ def df_string_cleaner(df: pd.DataFrame) -> pd.DataFrame:
 
     df = df.applymap(lowercase_and_strip)
 
-    # stripping and lowering case of all column and index values in df
+    # stripping and lowering case of all column and index values in df.
+    # 2023-06-14 12:15:44 this has the unfortunate side effect of converting all
+    # integer index vals to object. Add a test for numeric?
 
-    df.columns = df.columns.astype(str).str.strip().str.lower()
-    df.index = df.index.astype(str).str.strip().str.lower()
+    str_cols = df.columns.astype(str)
+
+    if not str_cols.str.isnumeric().all():
+        df.columns = str_cols.str.strip().str.lower()
+
+    str_idx = df.index.astype(str)
+    if not str_idx.str.isnumeric().all():
+        df.index = str_idx.str.strip().str.lower()
 
     return df
