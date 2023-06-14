5252a5a25d337e852b5c8f369845351cfea6a80b
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-15 00:49:41 +1000

    renamed var DB_DIR DB_PATH
    Former-commit-id: 782796f572c10539e67a4ffec6003e6da79012d2
    Former-commit-id: 8ff60983e56fb8d243863b6577dbaed749843fad

--- src/wine_analysis_hplc_uv/definitions.py
+++ src/wine_analysis_hplc_uv/definitions.py
@@ -2,7 +2,7 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-DB_DIR = os.environ.get("DB_DIR_PATH")
+DB_PATH = os.environ.get("DB_PATH_PATH")
 
 LIB_DIR = "/Users/jonathan/0_jono_data/mres_data_library/"
 
