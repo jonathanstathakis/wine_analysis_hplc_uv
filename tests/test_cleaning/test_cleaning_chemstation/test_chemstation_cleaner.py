1b695d24982755694d9cda2dd1b8e2304d91bf18
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-15 14:08:00 +1000

    add test for 4 digit id replacement. Add method to compare sampletracker and chemstation id cols to check for missing elements
    Former-commit-id: 73db81a39ea3555569bcf51b6b713eddffff64f7
    Former-commit-id: ed0abae7c90b461395a258aa0e6ded487e56fa9c

--- tests/test_cleaning/test_cleaning_chemstation/test_chemstation_cleaner.py
+++ tests/test_cleaning/test_cleaning_chemstation/test_chemstation_cleaner.py
@@ -18,7 +18,11 @@ from tests.mytestmethods.mytestmethods import test_report
 from tests.mytestmethods import test_methods_df
 from wine_analysis_hplc_uv.db_methods import db_methods
 from wine_analysis_hplc_uv.df_methods import df_cleaning_methods
-from wine_analysis_hplc_uv.chemstation import ch_metadata_tbl_cleaner
+from wine_analysis_hplc_uv.chemstation.ch_m_cleaner import (
+    ch_m_cleaner,
+    ch_m_date_cleaner,
+    ch_m_id_cleaner,
+)
 from wine_analysis_hplc_uv.definitions import DB_PATH, CH_META_TBL_NAME
 import dateutil.parser
 import pandas as pd
@@ -29,6 +33,8 @@ def clean_chm_tests(df: pd.DataFrame):
         (test_string_cleaner, df),
         (test_column_renames, df),
         (test_date_formatter, df),
+        (test_four_digit_id_to_two_digit, df),
+        # (test_id_cleaner, df),
     ]
     test_report(tests)
     return None
@@ -51,7 +57,7 @@ def test_column_renames(dirty_df: pd.DataFrame):
     Function renames the following: "notebook": "id", "date": "acq_date", "method": "acq_method"
     """
     cp_dirty_df = dirty_df.copy()
-    clean_df = ch_metadata_tbl_cleaner.rename_chemstation_metadata_cols(dirty_df)
+    clean_df = ch_m_cleaner.rename_chemstation_metadata_cols(dirty_df)
     assert not cp_dirty_df.equals(clean_df)
     assert not clean_df.columns.isin(["notebook", "date", "method"]).any()
     assert clean_df.columns.isin(["id", "acq_date", "acq_method"]).any()
@@ -61,7 +67,7 @@ def test_date_formatter(dirty_df: pd.DataFrame):
     """ """
     dirty_df = dirty_df.rename({"date": "acq_date"}, axis=1)
     cp_dirty_df = dirty_df.copy()
-    clean_df = ch_metadata_tbl_cleaner.format_acq_date(dirty_df)
+    clean_df = ch_m_date_cleaner.format_acq_date(dirty_df)
     # assert not dirty_df["acq_date"].equals(clean_df["acq_date"]), "no change"
     assert not cp_dirty_df["acq_date"].equals(
         clean_df["acq_date"]
@@ -75,6 +81,50 @@ def test_date_formatter(dirty_df: pd.DataFrame):
     check_format = clean_df["acq_date"].apply(lambda x: bool(pattern.match(x)))
 
     assert check_format.all(), "format doesnt match '%Y-%m-%d, %H:%M:%S'"
+
+
+def test_four_digit_id_to_two_digit(dirty_df: pd.DataFrame):
+    # make copy of original df
+    cp_dirty_df = dirty_df.copy()
+
+    # clean the string elements in the df - strip and lower
+    clean_df = df_cleaning_methods.df_string_cleaner(dirty_df)
+
+    # col renames
+    clean_df = ch_m_cleaner.rename_chemstation_metadata_cols(clean_df)
+
+    # add new_id col as a copy of "id"
+    clean_df["new_id"] = clean_df["id"]
+
+    # make a copy of the 'new_id' col after cleaning and renaming but before id replacement
+    cp_clean_df_new_id = clean_df["new_id"].copy()
+
+    # apply regex to capture ids matching the pattern 0NN1 and extract NN
+    clean_df["new_id"] = ch_m_id_cleaner.four_digit_id_to_two_digit(clean_df["new_id"])
+
+    # To test the effectiveness, get the same regex pattern used above
+    pattern = ch_m_id_cleaner.get_four_digit_code_regex()
+
+    # check if any codes in the old format are left over
+    def match_old_codes(x: str, pattern):
+        return bool(pattern.match(x))
+
+    old_code_match = clean_df["new_id"].apply(match_old_codes, args=[pattern])
+
+    # see if any matched
+    assert not old_code_match.any()
+
+    # final check, ensure that the copied col isnt equal to the new col
+    assert not cp_clean_df_new_id.equals(clean_df["new_id"])
+
+
+# def test_id_cleaner(dirty_df: pd.DataFrame):
+#     cp_dirty_df = dirty_df.copy()
+#     clean_df = df_cleaning_methods.df_string_cleaner(dirty_df)
+#     clean_df = ch_metadata_tbl_cleaner.rename_chemstation_metadata_cols(clean_df)
+#     clean_df = ch_metadata_tbl_cleaner.chemstation_id_cleaner(clean_df)
+
+#     assert not cp_dirty_df.equals(clean_df)
 
 
 def main():
