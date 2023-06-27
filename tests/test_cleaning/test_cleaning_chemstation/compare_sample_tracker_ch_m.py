8abc2f452ea14688b9afb5a7ab39df6b4b799181
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-28 09:30:42 +1000

    rewrite compare sample_tracker_ch_m as pytest mod

--- tests/test_cleaning/test_cleaning_chemstation/compare_sample_tracker_ch_m.py
+++ tests/test_cleaning/test_cleaning_chemstation/compare_sample_tracker_ch_m.py
@@ -1,23 +1,41 @@
 """
-Compare the contents of base chemstation metadata table with sample tracker table to identify what cleaning needs to be done to form a union.
+Compare the contents of base chemstation metadata table with sample tracker table to samplecodeentify what cleaning needs to be done to form a union.
 """
 from wine_analysis_hplc_uv.db_methods import db_methods
 from wine_analysis_hplc_uv.definitions import DB_PATH, CH_META_TBL_NAME, ST_TBL_NAME
 import pandas as pd
 
+import pytest
 
-def compare_columns(col1, col2):
-    comparison = col2[~(col2.isin(col1))]
+
+@pytest.fixture
+def get_st_df():
+    return db_methods.tbl_to_df(db_filepath=DB_PATH, tblname=ST_TBL_NAME)
+
+
+@pytest.fixture
+def get_ch_m_df():
+    return db_methods.tbl_to_df(db_filepath=DB_PATH, tblname=CH_META_TBL_NAME)
+
+
+@pytest.fixture
+def get_st_samplecode_col(get_st_df):
+    df = get_st_df
+    return df["samplecode"]
+
+
+@pytest.fixture
+def get_ch_m_notebook_col(get_ch_m_df):
+    return get_ch_m_df["notebook"]
+
+
+def test_compare_columns(get_ch_m_notebook_col, get_st_samplecode_col):
+    comparison = get_ch_m_notebook_col[
+        ~(get_ch_m_notebook_col.isin(get_st_samplecode_col))
+    ]
     print(comparison)
     print(comparison.shape)
 
 
-def main():
-    st_df = db_methods.tbl_to_df(db_filepath=DB_PATH, tblname=ST_TBL_NAME)
-    ch_m_df = db_methods.tbl_to_df(db_filepath=DB_PATH, tblname=CH_META_TBL_NAME)
-    ch_m_df = ch_m_df.rename({"notebook": "id"}, axis=1)
-    compare_columns(col1=st_df["id"], col2=ch_m_df["id"])
-
-
-if __name__ == "__main__":
-    main()
+# def main():
+#    compare_columns(col1=st_df["samplecode"], col2=ch_m_df["notebook"])
