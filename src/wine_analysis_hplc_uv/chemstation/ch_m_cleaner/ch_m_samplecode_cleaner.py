0d2e3d57b99c53d590b052801713503cb0baa5e8
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-29 00:57:36 +1000

    update ch_m_cleaner and associated tests to accomodate the full dataset at this point in time
    Former-commit-id: ba928663ae1436c601c29328d77fcef50bccc39c
    Former-commit-id: 48467793e49e4e4136e4c47683b1c56ce1b8bf23

--- src/wine_analysis_hplc_uv/chemstation/ch_m_cleaner/ch_m_samplecode_cleaner.py
+++ src/wine_analysis_hplc_uv/chemstation/ch_m_cleaner/ch_m_samplecode_cleaner.py
@@ -1,29 +1,34 @@
+from os import replace
 import pandas as pd
 import re
+from wine_analysis_hplc_uv.chemstation import logger
 
 
 def ch_m_samplecode_cleaner(df: pd.DataFrame) -> pd.DataFrame:
     """
-    1. create 'new_id' col based on 'id'
+    1. create 'join_samplecode' col based on 'id'
     2. rename 'id' col 'exp_id' to preserve that connection
     3.
     """
-    df["new_id"] = df["id"]
-    df = df.rename({"id": "exp_id"}, axis=1)
+    assert isinstance(df, pd.DataFrame)
+    df["join_samplecode"] = df["samplecode"]
+    df = df.rename({"samplecode": "ch_samplecode"}, axis=1)
 
-    print("cleaning chemstation run id's")
-    df = four_to_two_digit(series=df["new_id"])
-    df = string_samplecode_to_digit(df)
+    logger.debug(msg="cleaning chemstation run id's")
+    df["join_samplecode"] = four_to_two_digit(series=df["join_samplecode"])
+    df = replace_samplecodes(df)
+    df = replace_116_sigurd(df)
+    assert "2021-debortoli-cabernet-merlot_avantor" not in df["join_samplecode"]
+    assert isinstance(df, pd.DataFrame)
     return df
 
 
-def get_four_digit_code_regex():
-    return re.compile(
-        "^0(\d{2})1$"
-    )  # The pattern matches 4-digit strings starting with '0' and ending with '1'
-
-
 def four_to_two_digit(series: pd.Series) -> pd.DataFrame:
+    def get_four_digit_code_regex():
+        return re.compile(
+            "^0(\d{2})[12]$"
+        )  # The pattern matches 4-digit strings starting with '0' and ending with '1'
+
     pattern = get_four_digit_code_regex()
 
     def extract_middle_digits(x):
@@ -34,19 +39,45 @@ def four_to_two_digit(series: pd.Series) -> pd.DataFrame:
     return series
 
 
-# def four_digit_id_to_two_digit(series: pd.Series) -> pd.DataFrame:
-#     series = series.astype(str).apply(lambda x: x[1:3] if len(x) == 4 else x)
-#     return series
+def replace_116_sigurd(df: pd.DataFrame):
+    """
+    Modifies sigurd chenin blanc samplecode from 116 to sigurdcb, as there was a doubleup.
+    """
+    assert isinstance(df, pd.DataFrame)
+    assert not df.empty
+    # check that there are two 116s in the column
+    # if not, might not need this function
+    assert df["join_samplecode"].value_counts()["116"] == 2
+
+    replacement_str = "sigurdcb"
+
+    # mask for the row containing 'sigurd' in the description col
+    sigurd_mask = df["desc"].str.contains("sigurd", na=False)
+    # make sure there is at least 1 True value
+    assert sigurd_mask.isin([True]).any()
+    # make the value replacement
+    df.loc[sigurd_mask, "join_samplecode"] = replacement_str
+    # make sure there is now only 1 '116'
+    assert (
+        df[df["join_samplecode"] == "116"].shape[0] == 1
+    ), f"{df[df['join_samplecode'] == '116']}"
+    # make sure there is 1 'sigurdcb'
+    assert (
+        df[df["join_samplecode"] == replacement_str].shape[0] == 1
+    ), f"{df[df['join_samplecode'] == replacement_str]}"
+
+    return df
 
 
-def string_samplecode_to_digit(series: pd.Series) -> pd.DataFrame:
+def replace_samplecodes(series: pd.Series) -> pd.DataFrame:
     """
     Replaces the id of a number of runs with their 2 digit id's
     as stated in the sample tracker.
     """
 
     replace_dict = {
-        "2021-debortoli-cabernet-merlot_avantor|debertoli_cs": "72",
+        "mt-diff-bannock-pn": "mt-diff-bannockburn-pn",
+        "2021-debortoli-cabernet-merlot_avantor": "72",
         "stoney-rise-pn_02-21": "73",
         "crawford-cab_02-21": "74",
         "hey-malbec_02-21": "75",
--- tests/test_cleaning/test_cleaning_chemstation/test_chemstation_cleaner.py
+++ tests/test_cleaning/test_cleaning_chemstation/test_chemstation_cleaner.py
@@ -4,8 +4,9 @@ TODO:
 - [x] string cleaner
 - [x] column renames
 - [x] date formatter
-- [ ] new_ids
+- [ ] new_exp_samplecodes
 - [ ] remove marked runs.
+
 """
 import re
 import os
@@ -14,27 +15,28 @@ from wine_analysis_hplc_uv import db_methods
 
 sys.path.append("/Users/jonathan/mres_thesis/wine_analysis_hplc_uv/tests")
 from tests import test_logger
-from tests.mytestmethods.mytestmethods import test_report
-from tests.mytestmethods import test_methods_df
+from mydevtools.testing.mytestmethods import test_report
+from mydevtools.testing import test_methods_df
 from wine_analysis_hplc_uv.db_methods import db_methods
 from wine_analysis_hplc_uv.df_methods import df_cleaning_methods
 from wine_analysis_hplc_uv.chemstation.ch_m_cleaner import (
     ch_m_cleaner,
     ch_m_date_cleaner,
-    ch_m_id_cleaner,
+    ch_m_samplecode_cleaner,
 )
-from wine_analysis_hplc_uv.definitions import DB_PATH, CH_META_TBL_NAME
+from wine_analysis_hplc_uv.definitions import DB_PATH, CH_META_TBL_NAME, ST_TBL_NAME
 import dateutil.parser
 import pandas as pd
 
 
 def clean_chm_tests(df: pd.DataFrame):
     tests = [
-        (test_string_cleaner, df),
-        (test_column_renames, df),
-        (test_date_formatter, df),
-        (test_four_digit_id_to_two_digit, df),
-        # (test_id_cleaner, df),
+        # (test_string_cleaner, df),
+        # (test_column_renames, df),
+        # (test_date_formatter, df),
+        # (test_replace_116_sigurd, df),
+        # (test_four_digit_samplecode_to_two_digit, df),
+        (test_samplecode_cleaner, df),
     ]
     test_report(tests)
     return None
@@ -45,6 +47,15 @@ def get_test_db_path():
 
 
 def test_string_cleaner(dirty_df: pd.DataFrame):
+    """
+    test df_cleaning_methods.df_string_cleaner.
+    strictly speaking this should be in a seperate test module.
+
+    1. create a copy of the df, then 'clean' it.
+    2. assert that the two dfs are different.
+    3. assert that the clean_df does not have any trailing whitespace
+    4. assert that the clean_df does not have any uppercase characters.
+    """
     cp_dirty_df = dirty_df.copy()
     clean_df = df_cleaning_methods.df_string_cleaner(dirty_df)
     assert not cp_dirty_df.equals(clean_df)
@@ -54,13 +65,14 @@ def test_string_cleaner(dirty_df: pd.DataFrame):
 
 def test_column_renames(dirty_df: pd.DataFrame):
     """
-    Function renames the following: "notebook": "id", "date": "acq_date", "method": "acq_method"
+    Function renames the following:
+        "notebook": "samplecode", "date": "acq_date", "method": "acq_method"
     """
     cp_dirty_df = dirty_df.copy()
     clean_df = ch_m_cleaner.rename_chemstation_metadata_cols(dirty_df)
     assert not cp_dirty_df.equals(clean_df)
     assert not clean_df.columns.isin(["notebook", "date", "method"]).any()
-    assert clean_df.columns.isin(["id", "acq_date", "acq_method"]).any()
+    assert clean_df.columns.isin(["samplecode", "acq_date", "acq_method"]).any()
 
 
 def test_date_formatter(dirty_df: pd.DataFrame):
@@ -83,7 +95,7 @@ def test_date_formatter(dirty_df: pd.DataFrame):
     assert check_format.all(), "format doesnt match '%Y-%m-%d, %H:%M:%S'"
 
 
-def test_four_digit_id_to_two_digit(dirty_df: pd.DataFrame):
+def test_four_digit_samplecode_to_two_digit(dirty_df: pd.DataFrame):
     # make copy of original df
     cp_dirty_df = dirty_df.copy()
 
@@ -93,38 +105,96 @@ def test_four_digit_id_to_two_digit(dirty_df: pd.DataFrame):
     # col renames
     clean_df = ch_m_cleaner.rename_chemstation_metadata_cols(clean_df)
 
-    # add new_id col as a copy of "id"
-    clean_df["new_id"] = clean_df["id"]
+    # add new_samplecode col as a copy of "samplecode"
+    assert isinstance(clean_df, pd.DataFrame)
 
-    # make a copy of the 'new_id' col after cleaning and renaming but before id replacement
-    cp_clean_df_new_id = clean_df["new_id"].copy()
+    clean_df["new_samplecode"] = clean_df["samplecode"]
 
-    # apply regex to capture ids matching the pattern 0NN1 and extract NN
-    clean_df["new_id"] = ch_m_id_cleaner.four_digit_id_to_two_digit(clean_df["new_id"])
+    # make a copy of the 'new_samplecode' col after cleaning and renaming but before samplecode replacement
+    cp_clean_df_new_samplecode = clean_df["new_samplecode"].copy()
+
+    # apply regex to capture samplecodes matching the pattern 0NN1 and extract NN
+    clean_df["new_samplecode"] = ch_m_samplecode_cleaner.four_to_two_digit(
+        clean_df["new_samplecode"]
+    )
 
     # To test the effectiveness, get the same regex pattern used above
-    pattern = ch_m_id_cleaner.get_four_digit_code_regex()
+    pattern = ch_m_samplecode_cleaner.get_four_digit_code_regex()
 
     # check if any codes in the old format are left over
     def match_old_codes(x: str, pattern):
         return bool(pattern.match(x))
 
-    old_code_match = clean_df["new_id"].apply(match_old_codes, args=[pattern])
+    old_code_match = clean_df["new_samplecode"].apply(match_old_codes, args=[pattern])
 
     # see if any matched
     assert not old_code_match.any()
 
     # final check, ensure that the copied col isnt equal to the new col
-    assert not cp_clean_df_new_id.equals(clean_df["new_id"])
+    assert not cp_clean_df_new_samplecode.equals(clean_df["new_samplecode"])
 
 
-# def test_id_cleaner(dirty_df: pd.DataFrame):
-#     cp_dirty_df = dirty_df.copy()
-#     clean_df = df_cleaning_methods.df_string_cleaner(dirty_df)
-#     clean_df = ch_metadata_tbl_cleaner.rename_chemstation_metadata_cols(clean_df)
-#     clean_df = ch_metadata_tbl_cleaner.chemstation_id_cleaner(clean_df)
+def test_replace_116_sigurd(dirty_df: pd.DataFrame):
+    cp_dirty_df = dirty_df.copy()
+    clean_df = df_cleaning_methods.df_string_cleaner(dirty_df)
+    clean_df = ch_m_cleaner.rename_chemstation_metadata_cols(clean_df)
+    clean_df = ch_m_samplecode_cleaner.replace_116_sigurd(clean_df)
 
-#     assert not cp_dirty_df.equals(clean_df)
+    result = clean_df[clean_df["samplecode"] == "sigurdcb"]
+    assert not result.empty
+
+
+def test_samplecode_cleaner(dirty_df: pd.DataFrame):
+    """
+    Test the individual components of the ch_m_cleaner. The true test is that the difference between the sets of st samplecodes and ch_m samplecodes is zero.
+    """
+    cp_dirty_df = dirty_df.copy()
+    clean_df = df_cleaning_methods.df_string_cleaner(dirty_df)
+    clean_df = ch_m_cleaner.rename_chemstation_metadata_cols(clean_df)
+    assert "samplecode" in clean_df.columns
+    assert isinstance(clean_df, pd.DataFrame)
+    clean_df = ch_m_cleaner.ch_m_samplecode_cleaner.ch_m_samplecode_cleaner(clean_df)
+    assert (
+        clean_df[clean_df["join_samplecode"] == "116"].shape[0] == 1
+    ), f"{clean_df[clean_df['samplecode'] == '116']}"
+    # check for any change
+    assert not cp_dirty_df.equals(clean_df)
+
+    def compare_columns(col1, col2):
+        """
+        returns elements of Series col1 not in col2
+        """
+        # returns a boolean series of whether the values of col1 are present anywhere
+        # in col2
+        set_col1 = set(col1)
+        set_col2 = set(col2)
+
+        comparison = set_col1 - set_col2
+        return comparison
+
+    # get the values in ch_m.samplecode, not in st.sampelcode
+    st_samplecode = db_methods.tbl_to_df(DB_PATH, ST_TBL_NAME, "samplecode")
+    ch_m_samplecode = clean_df["join_samplecode"]
+    val_in_ch_m_not_st = compare_columns(ch_m_samplecode, st_samplecode)
+    print(val_in_ch_m_not_st)
+
+    pd.options.display.max_rows = 200
+    print(ch_m_samplecode)
+    val_in_st_not_in_ch_m = compare_columns(st_samplecode, ch_m_samplecode)
+    print(val_in_st_not_in_ch_m)
+
+    # a is ch_m, b is st, thus difference_ab is elements in a but not b, elements in
+    # ch_m but not st. difference_ba is elements in st but not ch_m
+
+    difference_ab = set(ch_m_samplecode).difference(st_samplecode)
+    difference_ba = set(st_samplecode).difference(ch_m_samplecode)
+    # pandas isin relies on np.in1d with default args. It tests whether each element of
+    # the first array is in the second.
+
+    pd_diff_ab = compare_columns(st_samplecode, ch_m_samplecode)
+
+    pd.options.display.max_rows = 200
+    # assert val_in_st_not_in_ch_m.shape[0] == 0, f"\nThe following are in st but not in ch_m:\n\n{val_in_st_not_in_ch_m}"
 
 
 def main():
