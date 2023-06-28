df84fa508d46560a6b5ad0ebc3e3670d6afeadd0
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-29 00:46:48 +1000

    add a template for writing db queries

---
+++ src/wine_analysis_hplc_uv/db_queries.py
@@ -0,0 +1,28 @@
+"""
+A module for writing db queries
+"""
+from wine_analysis_hplc_uv.definitions import DB_PATH, CH_META_TBL_NAME
+import duckdb as db
+
+
+def query_db(db_path: str, tbl_name: str):
+    query = """--sql
+    select
+        *
+    from
+        chemstation_metadata
+    where
+        "desc" like '%sigurd%';
+    """
+    with db.connect(db_path) as con:
+        con.sql(query).show()
+    return None
+
+
+def main():
+    return None
+
+
+if __name__ == "__main__":
+    query_db(DB_PATH, CH_META_TBL_NAME)
+    main()
