363f18b2cc3c7e08fb875c2a0b1e0b65fd35e72f
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-29 10:42:40 +1000

    ch_m cleaner defined as a class with internal methods, export to sheets or db
    Former-commit-id: 36a73ac00a33656e76f890eaa73c390c18c1fb99
    Former-commit-id: bc1b6f7654e823364a29c8d4efc577d00619fea5

--- src/wine_analysis_hplc_uv/chemstation/ch_m_cleaner/ch_m_cleaner.py
+++ src/wine_analysis_hplc_uv/chemstation/ch_m_cleaner/ch_m_cleaner.py
@@ -4,47 +4,53 @@ from wine_analysis_hplc_uv.chemstation.ch_m_cleaner import ch_m_date_cleaner
 from wine_analysis_hplc_uv.df_methods import df_cleaning_methods
 from wine_analysis_hplc_uv.db_methods import db_methods
 from wine_analysis_hplc_uv.generic import Exporter
+from wine_analysis_hplc_uv.definitions import (
+    DB_PATH,
+    CH_META_TBL_NAME,
+    CLEAN_CH_META_TBL_NAME,
+)
 
 
 class ChemstationCleaner(Exporter):
-    def c_ch_m_to_db(db_path, raw_tbl_name, clean_tbl_name):
-        """
-        1. Get the raw table as a df
-        2. clean the df
-        3. write the df to db
-        """
-        dirty_df = db_methods.tbl_to_df(db_filepath=db_path, tblname=raw_tbl_name)
-        # clean_df = ch_metadata_tbl_cleaner(df = dirty_df)
-        # db_methods.df_to_tbl(df=clean_df, tblname=clean_tbl_name, db_filepath=db_path)
-        return None
-
-    def ch_metadata_tbl_cleaner(df: pd.DataFrame) -> pd.DataFrame:
+    def ch_metadata_tbl_cleaner(self) -> pd.DataFrame:
         """
         driver function for cleaning chemstation metadata table to enable joins with sample_tracker table
         """
 
-        df = (
-            df.pipe(df_cleaning_methods.df_string_cleaner)
+        self.df = (
+            self.df.pipe(df_cleaning_methods.df_string_cleaner)
             .pipe(rename_chemstation_metadata_cols)
-            .pipe(ch_m_date_cleaner.format_acq_date())
+            .pipe(ch_m_date_cleaner.format_acq_date)
             .pipe(ch_m_samplecode_cleaner.ch_m_samplecode_cleaner)
             # .pipe(chemstation_metadata_drop_unwanted_runs)
         )
 
-        return df
+        return self.df
 
-    def rename_chemstation_metadata_cols(df):
-        original_names = ["notebook", "date", "method"]
-        new_names = ["samplecode", "acq_date", "acq_method"]
+    def __init__(self, db_path: str, raw_tbl_name: str):
+        self.db_path = db_path
+        self.raw_tbl_name = raw_tbl_name
 
-        rename_dict = dict(zip(original_names, new_names))
-        df = df.rename(rename_dict, axis=1)
-        assert not df.columns.isin(original_names).all()
-        assert df.columns.isin(new_names).any()
-        return df
+        self.df = db_methods.tbl_to_df(self.db_path, self.raw_tbl_name)
+        self.clean_df = self.ch_metadata_tbl_cleaner()
+
+
+def rename_chemstation_metadata_cols(df):
+    """ """
+    original_names = ["notebook", "date", "method"]
+    new_names = ["samplecode", "acq_date", "acq_method"]
+
+    rename_dict = dict(zip(original_names, new_names))
+    df = df.rename(rename_dict, axis=1)
+    assert not df.columns.isin(original_names).all()
+    assert df.columns.isin(new_names).any()
+    return df
 
 
 def main():
+    ch_cleaner = ChemstationCleaner(DB_PATH, CH_META_TBL_NAME)
+    ch_cleaner.to_db(db_filepath=DB_PATH, tbl_name=CLEAN_CH_META_TBL_NAME)
+
     return None
 
 
