3c4f2d51b8c66db3846f8baa290f23fd7c654ea1
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-29 10:23:38 +1000

    moved Exporter class into new generic module, updated cellartracker tests
    Former-commit-id: f11c3fc40c79586fcb32c28ef018161dd5eace35
    Former-commit-id: 6b9c9eff029713a769643b25dd9825e8c4ab5bc7

--- src/wine_analysis_hplc_uv/cellartracker_methods/my_cellartracker_class.py
+++ src/wine_analysis_hplc_uv/cellartracker_methods/my_cellartracker_class.py
@@ -6,36 +6,7 @@ import pandas as pd
 from wine_analysis_hplc_uv.my_sheetsinterface import gspread_methods as g_methods
 import duckdb as db
 from wine_analysis_hplc_uv.db_methods import db_methods
-
-
-class Exporter:
-    """
-    A class to contain df -> sheets and db methods.
-    """
-
-    def __init__(self):
-        self.df = None
-
-    def to_sheets(self, key: str, sheet_title: str):
-        """
-        Output the self.df to a given google sheets sheet for a provided key and
-        sheet_title. If the sheet doesnt exist in the Sheet, it will be created.
-        """
-        assert isinstance(key, str)
-        assert isinstance(sheet_title, str)
-        self.sheets_key = key
-        wksh = g_methods.WorkSheet(key=key, sheet_title=sheet_title)
-        wksh.sheet_df = self.df
-        wksh.write_to_sheet()
-
-    def to_db(self, db_filepath: str, tbl_name: str):
-        """
-        Output contained self.df to designated database given by filepath.
-        """
-        df = self.df
-
-        with db.connect(database=db_filepath) as con:
-            con.sql(query=f"CREATE TABLE {tbl_name} AS SELECT * FROM df")
+from wine_analysis_hplc_uv.generic import Exporter
 
 
 class MyCellarTracker(cellartracker.CellarTracker, Exporter):
