b73460c98a642030f17cf0c20f42ffcca9790c2c
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-29 13:49:20 +1000

    build_library updated with current methods and tested
    Former-commit-id: 0f4b5e91880b148f2810029562817d12ea9d967d
    Former-commit-id: 6a9fb91ffe9099f938f4dc3a98b9003179fd94f2

--- src/wine_analysis_hplc_uv/core/build_library.py
+++ src/wine_analysis_hplc_uv/core/build_library.py
@@ -7,13 +7,14 @@ import shutil
 import duckdb as db
 
 from wine_analysis_hplc_uv.core import adapt_super_pipe_to_db
-from wine_analysis_hplc_uv.sampletracker import sample_tracker_processor
-
-from wine_analysis_hplc_uv.definitions import DB_DIR, LIB_DIR
+from wine_analysis_hplc_uv.sampletracker.st_cleaner import STCleaner
+from wine_analysis_hplc_uv.chemstation.ch_m_cleaner.ch_m_cleaner import (
+    ChemstationCleaner,
+)
+from wine_analysis_hplc_uv.cellartracker_methods.ct_cleaner import CTCleaner
+from wine_analysis_hplc_uv import definitions
 from wine_analysis_hplc_uv.core import ch_to_db, st_to_db, ct_to_db
-
 import pandas as pd
-
 import logging
 
 chemstation_logger = logging.getLogger("wine_analysis_hplc_uv.chemstation")
@@ -28,20 +29,20 @@ def build_db_library(data_lib_path: str, db_path: str) -> None:
 
     # 1. create db file if none exists.
     # dont use context management here because simply opening and closing a connection, forcing the creation of the .db file
-    if not os.path.isfile(DB_DIR):
-        con = db.connect(DB_DIR)
+    if not os.path.isfile(db_path):
+        con = db.connect(db_path)
         con.close()
 
     #  3. write raw tables to db from sources
-    mtadta_tbl = "chemstation_metadata"
-    sc_tbl = "chromatogram_spectra"
-    st_tbl = "sample_tracker"
-    ct_tbl = "cellar_tracker"
-    super_tbl_name = "super_table"
+    mtadta_tbl = definitions.CH_META_TBL_NAME
+    ch_d_tbl = definitions.CH_DATA_TBL_NAME
+    st_tbl = definitions.ST_TBL_NAME
+    ct_tbl = definitions.CT_TBL_NAME
+    # super_tbl_name = "super_table"
 
     # chemstation
     ch_to_db.ch_to_db(
-        lib_path=data_lib_path, mtadata_tbl=mtadta_tbl, sc_tbl=sc_tbl, db_path=db_path
+        lib_path=data_lib_path, mtadata_tbl=mtadta_tbl, sc_tbl=ch_d_tbl, db_path=db_path
     )
 
     # sample_tracker
@@ -53,6 +54,17 @@ def build_db_library(data_lib_path: str, db_path: str) -> None:
     un = os.environ.get("CELLAR_TRACKER_UN")
     pw = os.environ.get("CELLAR_TRACKER_PW")
     ct_to_db.ct_to_db(db_filepath=db_path, ct_tbl=ct_tbl, un=un, pw=pw)
+
+    # cleaners
+    ## ch_m
+    ch_m_cleaner = ChemstationCleaner(db_path=db_path, raw_tbl_name=mtadta_tbl)
+    ch_m_cleaner.to_db(db_filepath=db_path, tbl_name=definitions.CLEAN_CH_META_TBL_NAME)
+    ## st
+    st_cleaner = STCleaner(db_path=db_path, raw_tbl_name=st_tbl)
+    st_cleaner.to_db(db_filepath=db_path, tbl_name=definitions.CLEAN_ST_TBL_NAME)
+    ## ct
+    ct_cleaner = CTCleaner(db_path=db_path, raw_tbl_name=ct_tbl)
+    ct_cleaner.to_db(db_filepath=db_path, tbl_name=definitions.CLEAN_CT_TBL_NAME)
 
     # # 5. join the tables together.
     # ux.ask_user_and_execute(
@@ -76,9 +88,11 @@ def remove_existing_db(db_path: str) -> None:
 
 
 def main():
-    data_lib_path = LIB_DIR
-    db_filepath = DB_DIR
-    build_db_library(data_lib_path=data_lib_path, db_path=db_filepath)
+    data_lib_path = definitions.LIB_DIR
+    db_filepath = os.path.dirname(os.path.abspath(__file__))
+
+    print(db_filepath)
+    # build_db_library(data_lib_path=data_lib_path, db_path=db_filepath)
 
 
 if __name__ == "__main__":
---
+++ tests/test_build_library.py
@@ -0,0 +1,30 @@
+"""
+TODO:
+- [ ] add automation
+"""
+
+import pytest
+
+from wine_analysis_hplc_uv import definitions
+from wine_analysis_hplc_uv.core import build_library
+import os
+
+
+@pytest.fixture
+def get_test_db_path():
+    dirpath = os.path.dirname(os.path.abspath(__file__))
+    db_filename = "test_build_library.db"
+    fullpath = os.path.join(dirpath, db_filename)
+    print(fullpath)
+    return fullpath
+
+
+@pytest.fixture
+def get_datalibpath():
+    return definitions.LIB_DIR
+
+
+def test_build_library(get_test_db_path, get_datalibpath):
+    build_library.build_db_library(
+        data_lib_path=get_datalibpath, db_path=get_test_db_path
+    )
