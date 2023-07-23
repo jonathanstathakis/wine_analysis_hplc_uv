fbfcec17add317c9ea37a1f30a61bf9b37eb1f34
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-07-23 11:41:35 +1000

    lib eda update
    Former-commit-id: e11a5059b3e73abf5fda300c0393c9911d4d96e0
    Former-commit-id: 6598c57fede7c68bfa14dffa28900c007573f38a

--- src/wine_analysis_hplc_uv/library_eda/lib_eda/lib_eda.py
+++ src/wine_analysis_hplc_uv/library_eda/lib_eda/lib_eda.py
@@ -14,218 +14,190 @@ categories:
 """
 
 import pandas as pd
-import logging
 import numpy as np
 
 
-def calc_prop(series, tot_size):
-    return series / tot_size * 100
+def get_test_df():
+    # 2020 noname shiraz has 1 raw, 1 cuprac
+    # 2021 alphabet chard has 1 cuprac
+    # 2018 zwelt riesling has 1 raw
+    # 2000 blewitt springs moscato has 2 raw
+
+    test_df = pd.DataFrame(
+        dict(
+            wine=[
+                "2020 noname shiraz",
+                "2020 noname shiraz",
+                "2020 noname shiraz",
+                "2021 alphabet chard",
+                "2018 zwelt riesling",
+                "2000 blewitt springs",
+                "2000 blewitt springs",
+            ],
+            detection=[
+                "cuprac",
+                "raw",
+                "raw",
+                "cuprac",
+                "raw",
+                "raw",
+                "raw",
+            ],
+        )
+    )
+    return test_df
 
 
-def count_subpop_detection(in_df):
-    # Counts of rows by detection type
-    out_df = in_df.groupby("detection").size()
-    out_df = out_df.reset_index(name="n")
-    out_df = out_df.set_index("detection")
-    out_df["prop"] = 100.00
-    mindex = pd.MultiIndex.from_product([out_df.index, ["subpop_size"]])
-    out_df.index = mindex
-    out_df = out_df
-    return out_df
-
-
-def count_individuals(in_df):
-    # num individual samples in data set
-    out_df = in_df.groupby(["detection"])["wine"].nunique()
-    out_df = out_df.reset_index(name="n")
-    out_df = out_df.set_index("detection")
-    out_df["prop"] = np.round(out_df["n"] / in_df.shape[0], 2)
-    out_df["prop"] = calc_prop(out_df["n"], in_df.shape[0])
-    mindex = pd.MultiIndex.from_product([out_df.index, ["count_individs"]])
-    out_df.index = mindex
-
-    return out_df
-
-
-def count_unrepeated_samples(in_df):
+def samples_without_repeats(in_df):
     # count of samples without repeats
-    # get the count of repetitions of each sample
-    vcount_df = (
-        in_df.groupby(["detection"])["wine"]
-        .value_counts()
-        .reset_index()
-        .rename({"count": "n"}, axis=1)
+    unrepeated_samples_df = (
+        in_df.groupby(["detection", "wine"])
+        .size()
+        .to_frame(name="n")
+        .loc[lambda x: x["n"] == 1]
     )
-    # filter out any who has a repetition greater than 1, i.e. 1 occurance
-    unique_df = vcount_df[vcount_df["n"] == 1]
-    # get the count of rows remaining after filtering
 
-    out_df = (
-        unique_df.groupby(["detection"])["wine"]
-        .count()
-        .reset_index(name="n")
-        .set_index("detection")
-    )
-    out_df["prop"] = calc_prop(out_df["n"], in_df.shape[0])
-    mindex = pd.MultiIndex.from_product([out_df.index, ["count_unrep"]])
-    out_df.index = mindex
-    return out_df
+    return unrepeated_samples_df
 
 
-def count_repeated_samples(in_df):
-    # count of individuals who have at least one repetition
-    vcount_df = (
-        in_df.groupby(["detection"])["wine"]
-        .value_counts()
-        .reset_index()
-        .rename({"count": "n"}, axis=1)
-    )
-    dup_df = vcount_df[vcount_df["n"] > 1]
-    out_df = (
-        dup_df.groupby(["detection"])["wine"]
-        .count()
-        .reset_index(name="n")
-        .set_index("detection")
-    )
-    out_df["prop"] = calc_prop(out_df["n"], in_df.shape[0])
-    mindex = pd.MultiIndex.from_product([out_df.index, ["count_reps"]])
-    out_df.index = mindex
-    return out_df
-
-
-def size_tot_repeats(in_df):
+def samples_with_repeats(in_df):
     # total count of inviduals in data set who are repetitions
-    out_df = in_df.groupby(["detection", "wine"]).size().reset_index(name="n")
-    out_df = out_df[out_df["n"] > 1]
-    out_df = out_df.groupby("detection")["n"].sum().reset_index()
-    out_df["prop"] = calc_prop(out_df["n"], in_df.shape[0])
-    out_df = out_df.set_index("detection")
-    mindex = pd.MultiIndex.from_product([out_df.index, ["size_repeats"]])
-    out_df.index = mindex
-    return out_df
+    inter_df = in_df
+
+    repeats = (
+        inter_df.set_index(["detection", "wine"])
+        .index.value_counts()
+        .loc[lambda s: s > 1]
+        .to_frame(name="n")
+    )
+    return repeats
+
+
+# TODO:convert the other description functions into this format further use down the track then aggregate their values into the summary tabl
 
 
 #######################################################################################
 
 
 def summary_table(df):
-    n = count_subpop_detection(df)
-    count_individuals_df = count_individuals(df)
-    count_unrepeated_samples_df = count_unrepeated_samples(df)
-    count_repeated_samples_df = count_repeated_samples(df)
-    size_repeated_df = size_tot_repeats(df)
+    samples_with_repeats_df = samples_with_repeats(df)
+
+    unrepeated_samples_df = samples_without_repeats(df)
+
+    # num samples per detect cat
+    num_samples_by_detect = df.groupby("detection").size().rename("tot_samples")
+
+    # individual wines
+    ind_df = df.loc[:, ["detection", "wine"]].drop_duplicates()
+
+    # number of individual wines per detect method
+    num_ind_by_detect = ind_df.groupby(["detection"]).size().rename("n_ind_samples")
+
+    # num samples with repeats
+    num_samples_w_repeat = (
+        samples_with_repeats_df.groupby("detection")
+        .size()
+        .rename("num_samples_w_repeat")
+    )
+
+    # number of wines without repeats
+    num_samples_no_repeat = (
+        unrepeated_samples_df.groupby("detection")
+        .size()
+        .rename("num_samples_no_repeat")
+    )
+
+    # total number of repeats per detect cat
+    num_repeated_per_detect = (
+        samples_with_repeats_df.groupby("detection")
+        .sum()
+        .rename({"n": "tot_num_repeats"}, axis=1)
+    )
+
+    # to enter into the summary table need to be series with index detect, values 'n'
+    # formed by .groupby(['detection'].size().rename("measure"))
 
     summary_df = pd.concat(
         [
-            n,
-            count_individuals_df,
-            count_repeated_samples_df,
-            count_unrepeated_samples_df,
-            size_repeated_df,
+            num_samples_by_detect,
+            num_ind_by_detect,
+            num_samples_w_repeat,
+            num_samples_no_repeat,
+            num_repeated_per_detect,
         ],
-        axis=0,
+        axis=1,
+    ).rename_axis("statistic", axis="columns")
+
+    summary_pivot_df = (
+        summary_df.T.reset_index()
+        .melt(id_vars="statistic")
+        .pivot_table(
+            columns="detection",
+            index="statistic",
+            values="value",
+            margins=True,
+            margins_name="total",
+            aggfunc=sum,
+            sort=False,
+        )
+        .drop("total")  # total row is meaningless in this context
     )
 
-    return summary_df
+    return summary_pivot_df
 
 
 #######################################################################################
 
 
-def add_descriptor_columns(df):
-    out_df = df.copy()
-    out_df["cumsum"] = out_df["count"].cumsum()
-    out_df["prop"] = calc_prop(out_df["count"], out_df["count"].sum())
-    out_df.loc["total"] = out_df.sum()
-    out_df["cumsum_prop"] = out_df["prop"].cumsum()
-    out_df["prop"] = out_df["prop"].apply(np.round, 2)
-    out_df["cumsum_prop"] = out_df["cumsum_prop"].apply(np.round, 2)
-    out_df["count"] = out_df["count"].astype(int)
-    out_df["cumsum"] = out_df["cumsum"].apply(int)
-    out_df["cumsum"] = out_df["cumsum"].astype(str)
-    out_df.loc["total", "cumsum"] = out_df["cumsum"].iloc[-2]
-    out_df.loc["total", "cumsum_prop"] = out_df["cumsum_prop"].iloc[-2]
-
-    if "detection" in out_df.columns:
-        out_df.loc["total", "detection"] = np.nan
-    out_df = out_df.fillna("")
-    return out_df
+#######################################################################################
 
 
-def show_barplot(df, **kwargs):
-    return df["count"].plot.bar(**kwargs)
+def detection_intersection(df):
+    # find the intersection of detection methods,
+    # i.e. samples that were detected under both configs
 
-
-def num_unique_wines_by_detect_by_variety(in_df):
-    # The counts of each varietal by detection
-    detection = in_df["detection"].iloc[0]
-    out_df = (
-        in_df.groupby(["varietal"])["wine"]
-        .nunique()
-        .reset_index()
-        .rename({"wine": "count"}, axis=1)
-        .sort_values(["count"], ascending=[False])
-        .set_index("varietal")
+    both_detect_df = (
+        df.loc[:, ["detection", "vintage", "name_ct"]]
+        .assign(wine=lambda x: x["vintage"] + " " + x["name_ct"])
+        .drop(["vintage", "name_ct"], axis=1)
+        .groupby("wine")
+        .filter(lambda x: x["detection"].nunique() >= 2)
+        .reset_index(drop=True)
+        .groupby(["wine", "detection"])
+        .size()
+        .reset_index(name="n")
+        .pivot_table(
+            index="wine",
+            columns="detection",
+            values="n",
+            margins=True,
+            margins_name="total",
+            aggfunc=sum,
+        )
+        .assign(is_total=lambda x: x.index == "total")
+        .sort_values(["total"], ascending=False)
+        .sort_values(["is_total"])
+        .drop("is_total", axis=1)
+        .reindex()
     )
-    out_df["count"] = out_df["count"].astype(int)
-    out_df["detection"] = detection
-    out_df = add_descriptor_columns(out_df)
-    return out_df
+    return both_detect_df
 
 
-def num_unique_wines_by_detect_by_type(in_df):
-    # count of unique wines by type
-    df = in_df.copy()
+def main():
+    import os
 
-    out_df = (
-        df.groupby(["type"])[["wine"]]
-        .nunique()
-        .reset_index()
-        # .sort_values(["wine", 'type'], ascending=[False, False])
-        .rename({"wine": "count"}, axis=1)
-        # .set_index('type')
+    df = (
+        pd.read_excel(
+            os.path.join(os.path.dirname(__file__), "..", "df.xlsx"), dtype=object
+        )
+        .rename({"vintage_ct": "vintage"}, axis=1)
+        .assign(wine=lambda x: x["vintage"] + " " + x["name_ct"])
+        .loc[lambda x: ~x["wine"].isna()]
     )
-    order = [
-        "white - sparkling",
-        "rosé - sparkling",
-        "white",
-        "orange",
-        "rosé",
-        "red",
-        "white - sweet/dessert",
-    ]
-    out_df["type"] = pd.Categorical(out_df["type"], categories=order, ordered=True)
-    out_df = out_df.sort_values("type", ascending=True)
-    out_df = out_df.set_index("type")
-    out_df.index = out_df.index.astype(str)
-    return out_df
+    assert not df.empty
+
+    summary_table(df)
 
 
-def num_unique_wines_by_detect_by_country(in_df):
-    # count of unique wines by country
-    detection = in_df["detection"].iloc[0]
-    out_df = (
-        in_df.groupby(["country"])[["wine"]]
-        .nunique()
-        .reset_index()
-        .sort_values(["wine", "country"], ascending=[False, True])
-        .rename({"wine": "count"}, axis=1)
-    )
-    out_df["detection"] = detection
-    return out_df
-
-
-def num_unique_wines_by_detect_by_vintage(in_df):
-    # count of unique wines by vintage
-    in_df["vintage"] = in_df["vintage"].astype(int)
-
-    out_df = (
-        in_df.groupby(["detection", "vintage"])[["wine"]]
-        .nunique()
-        .reset_index(["vintage", "detection"])
-        .sort_values(["vintage", "wine", "detection"], ascending=[False, False, False])
-    )
-    out_df = out_df.rename({"wine": "count"}, axis=1)
-    out_df["count"] = out_df["count"].astype(int)
-
-    return out_df
+if __name__ == "__main__":
+    main()
