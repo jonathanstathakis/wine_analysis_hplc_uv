e6e9aaf0f94201b9caae074140a2a580a52d597b
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-07-24 11:21:32 +1000

    setting up pca environment
    Former-commit-id: 6e1bfc86f181d1de7d8c85d955320783c77f42ac
    Former-commit-id: 3cc533869de7be6d40a380085d914ba7652e44f7

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-its a-me
+# Project README
+
+
+
+## PCA Modelling
+
+PCA modelling package is [here](src/wine_analysis_hplc_uv/modeling/pca.py)
--- tests/test_modeling/test_pca/test_pca.py
+++ tests/test_modeling/test_pca/test_pca.py
@@ -1,17 +1,28 @@
 import pytest
 import logging
 import duckdb as db
-from wine_analysis_hplc_uv import definitions
+from wine_analysis_hplc_uv import db_methods, definitions
+import wine_analysis_hplc_uv
+from wine_analysis_hplc_uv.modeling import pca
 
 
 @pytest.fixture
-def get_dataset():
+def get_con():
+    return db.connect(definitions.DB_PATH)
+
+
+@pytest.fixture
+def get_sc_tbl(get_con):
+    return pca.get_sc_rel(get_con)
+
+
+def test_get_sc_tbl(get_sc_tbl):
+    assert get_sc_tbl
+
+
+def test_sc_rel():
+    from wine_analysis_hplc_uv.db_methods import db_methods
+
     con = db.connect(definitions.DB_PATH)
-
-    super_tbl_rel = con.sql("SELECT * FROM super_tbl").show()
-    print(super_tbl_rel)
-
-
-def test_pca(get_dataset):
-    a = get_dataset
-    print(a)
+    rel = db_methods.get_sc_rel(con)
+    assert not rel.df().empty
