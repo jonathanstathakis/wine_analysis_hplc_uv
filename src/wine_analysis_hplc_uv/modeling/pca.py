6e1bfc86f181d1de7d8c85d955320783c77f42ac
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-07-24 11:21:32 +1000

    setting up pca environment

--- src/wine_analysis_hplc_uv/modeling/pca.py
+++ src/wine_analysis_hplc_uv/modeling/pca.py
@@ -1,3 +1,11 @@
 """
 
 """
+
+import duckdb as db
+from wine_analysis_hplc_uv.db_methods import db_methods
+from wine_analysis_hplc_uv import definitions
+
+
+def get_sc_rel(con):
+    return db_methods.get_sc_rel(con)
