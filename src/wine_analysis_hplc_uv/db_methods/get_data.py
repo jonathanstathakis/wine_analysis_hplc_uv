207958952cdb63190827b9b22b1f4bdf1df7039b
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-08-09 08:57:47 +1000

    update get_data and tests to include optional parameters
    Former-commit-id: 5c54cc5d8fe4889753ac2a046b02d07b440aba43
    Former-commit-id: ff81f1a34476c6f2badcd0035ebd92599d5b1cf1

--- src/wine_analysis_hplc_uv/db_methods/get_data.py
+++ src/wine_analysis_hplc_uv/db_methods/get_data.py
@@ -6,7 +6,16 @@ logger = logging.getLogger(__name__)
 
 
 @timeit
-def get_wine_data(con):
+def get_wine_data(
+    con,
+    detection=None,
+    samplecode=None,
+    color=None,
+    wavelength=None,
+    varietal=None,
+    wine=None,
+    mins: tuple = (None, None),
+):
     """
     Select all CUPRAC wines, getting their metadata as well by first joining the
     sample tracker and cellar tracker tables, then chemstation metadata then finally
@@ -60,128 +69,47 @@ def get_wine_data(con):
     (66, 6)
     """
 
-    # add original st shape then each filter to show reduction in size..
-
-    logger.info(
-        "Fetching CUPRAC wine data via joins of st-ct, then st-ct-ch_m, then st-ct-ch_m-ch_d.."
-    )
-
-    logger.info(f"st has shape: {con.sql('SELECT * FROM c_sample_tracker').df().shape}")
-
-    st_cuprac = con.sql(
-        """--sql
-                        CREATE OR REPLACE TEMPORARY TABLE
-                        st_cuprac
-                        AS
-                        SELECT *
-                        FROM
-                        c_sample_tracker
-                        WHERE
-                        detection='cuprac'
-                        ;
-                        SELECT * FROM st_cuprac
-                        """
-    ).df()
-    logger.info(f"After filtering for CUPRAC wines, st has shape: {st_cuprac.shape}")
-
-    logger.info("joinng st to ct..")
-    logger.info(f"ct shape: {con.sql('SELECT * FROM c_cellar_tracker').df().shape}..")
-
-    """
-    
-    """
-
     # join st to ct
-    st_ct_temp = con.sql(
+    con.execute(
         """--sql
-        CREATE OR REPLACE TEMPORARY TABLE
-        st_ct_temp
-        AS
-        SELECT
-        st.detection, st.samplecode, ct.wine, ct.color, ct.varietal
-        FROM
-        st_cuprac st
-        INNER JOIN
-        c_cellar_tracker ct
-        ON
-        st.ct_wine_name = ct.wine
+        CREATE OR REPLACE TEMPORARY TABLE wine_data AS
+        SELECT st.detection,
+            st.samplecode,
+            ct.wine,
+            ct.color,
+            ct.varietal,
+            chm.id,
+            cs.mins,
+            cs.wavelength,
+            cs.value
+        FROM c_sample_tracker st
+            INNER JOIN c_cellar_tracker ct ON st.ct_wine_name = ct.wine
+            LEFT JOIN c_chemstation_metadata chm ON (
+                chm.join_samplecode = st.samplecode
+            )
+            LEFT JOIN chromatogram_spectra cs ON (chm.id = cs.id)
         WHERE
-        ct.color='red'
-        ;
-        SELECT * FROM st_ct_temp;
-    """
-    ).pl()
-
-    logger.info(
-        f"After st-ct inner join of red wines, the resulting table has shape: {st_ct_temp.shape}.."
+            st.detection = COALESCE($detection, st.detection)
+            AND st.samplecode =  COALESCE($samplecode, st.samplecode)
+            AND ct.color= COALESCE($color, ct.color)
+            AND ct.varietal= COALESCE($varietal, ct.varietal)
+            AND ct.wine=COALESCE($wine, ct.wine)
+            AND cs.wavelength = COALESCE($wavelength, cs.
+            wavelength)
+            AND cs.mins >= COALESCE($min_start, cs.mins)
+            AND cs.mins <= COALESCE($min_end, cs.mins)
+        """,
+        {
+            "detection": detection,
+            "samplecode": samplecode,
+            "color": color,
+            "varietal": varietal,
+            "wavelength": wavelength,
+            "wine": wine,
+            "min_start": mins[0],
+            "min_end": mins[1],
+        },
     )
-
-    # join st_ct_join to metadata tbl. Need to do a left join on st_ct_temp to select the samples that we've already filtered for i.e. by detection method.
-
-    logger.info(f"Now joining st-ct to ch_m..")
-    logger.info(
-        "before join, ch_m has"
-        f" shape: {con.sql('SELECT * FROM c_chemstation_metadata').df().shape}.."
-    )
-
-    ch_m_st_ct_temp = con.sql(
-        """--sql
-        CREATE OR REPLACE TEMPORARY TABLE
-        ch_m_st_ct_temp
-        AS
-        SELECT
-        t.detection, t.samplecode, t.wine, t.color, t.varietal, chm.id
-        FROM
-        st_ct_temp t
-        LEFT JOIN
-        c_chemstation_metadata chm
-        ON
-        (
-        chm.join_samplecode=t.samplecode
-        );
-        """
-    )
-
-    logger.info(
-        "after st-ct join to ch_m, resulting table has shape:"
-        f" {con.sql('SELECT * FROM ch_m_st_ct_temp;').pl().shape}.."
-    )
-
-    # ch_m_st_ct_ch_d
-
-    logger.info("joining ch_m-st_ct_temp and ch_d..")
-
-    # cs columns: id, mins, wavelength, value
-    wine_data = con.sql(
-        """--sql
-        CREATE OR REPLACE TEMPORARY TABLE
-        wine_data
-        AS
-        SELECT
-        t.detection, t.samplecode, t.wine, t.color, t.varietal, t.id, cs.mins, cs.wavelength, cs.value
-        FROM
-        ch_m_st_ct_temp t
-        LEFT JOIN
-        chromatogram_spectra cs
-        ON
-        (
-            t.id=cs.id
-        )
-        WHERE
-        cs.wavelength=450
-        AND
-        cs.mins<30
-        ;
-        SELECT * FROM wine_data;
-        """
-    ).pl()
-
-    logger.info(f"after join, wine_data has shape:{wine_data.shape}..")
-    # logger.info(
-    #     f"after join, wine_data has na in the following:\n{wine_data.isna().sum()}.."
-    # )
-
-    return wine_data
 
 
 def main():
--- tests/test_modeling/test_pca/test_get_data.py
+++ tests/test_modeling/test_pca/test_get_data.py
@@ -1,3 +1,4 @@
+from numpy import core
 from wine_analysis_hplc_uv.db_methods import get_data
 import pytest
 import pandas as pd
@@ -12,18 +13,16 @@ def gda():
         """
 
         def __init__(self):
-            self.detection = ["'cuprac'", "raw"]
+            self.detection = ["cuprac", "raw"]
             self.samplecode = ["116", "111", "148", "90"]
             self.wine = [
                 "2018 crawford river cabernets",
-                "2014 perrier-jouët champagne belle epoque",
-                "2020 vulka etna bianco cantine nicosia etna bianco",
                 "2022 vinden estate the vinden headcase pokolbin blanc",
                 "2019 shaw and smith shiraz balhannah",
             ]
-            self.color = ["red", "white"]
+            self.color = ["red", "white", "orange"]
             self.varietal = [
-                "carignan blend",
+                # "carignan blend",
                 "cabernet-shiraz blend",
                 "chardonnay",
                 "riesling",
@@ -33,6 +32,7 @@ def gda():
             self.wavelength = [
                 254,
                 190,
+                # (254, 190)
             ]
             # could add more here, this is a difficult problem. MVP means I should leave it as is,
             # can build up to slicing later down the track.
@@ -42,23 +42,81 @@ def gda():
     return gda
 
 
+def cat_var_assertions(corecon, keyword):
+    wine_data_shape = corecon.sql(
+        "select table_name, estimated_size, column_count from duckdb_tables where table_name='wine_data'"
+    ).pl()
+    print(wine_data_shape)
+    assert wine_data_shape["estimated_size"][0] > 1
+    assert wine_data_shape["column_count"][0] > 1
+
+    # check that only 1 keyword value present
+    tbl_detection_val = corecon.execute(
+        f"SELECT DISTINCT $keyword from wine_data", {"keyword": keyword}
+    ).fetchall()
+    assert len(tbl_detection_val) == 1
+    assert tbl_detection_val[0][0] == keyword
+
+
 def test_samplecode(corecon, gda):
     # test samplecode
     for samplecode in gda.samplecode:
-        wine_data = get_data.get_wine_data(corecon, samplecode=samplecode)
-        print(wine_data.head())
-
-        assert not wine_data.empty, samplecode
-        assert wine_data.isna().sum().sum() == 0, samplecode
+        print(samplecode)
+        get_data.get_wine_data(corecon, samplecode=samplecode)
+        cat_var_assertions(corecon, samplecode)
 
 
 def test_detection(corecon, gda):
     for detection in gda.detection:
-        wine_data = get_data.get_wine_data(corecon, detection=detection)
+        get_data.get_wine_data(corecon, detection=detection)
 
-        assert not wine_data.empty, detection
-        assert wine_data.isna().sum().sum() == 0, detection
-        assert (
-            wine_data["detection"].drop_duplicates().at[0] == detection
-            or wine_data["detection"].isin(["cuprac", "raw"]).all()
-        ), wine_data["detection"]
+        # check that rows and columns are greater than 1
+        cat_var_assertions(corecon, detection)
+
+
+def test_color(corecon, gda):
+    for color in gda.color:
+        print(color)
+        get_data.get_wine_data(corecon, color=color)
+
+        cat_var_assertions(corecon, color)
+
+
+def test_wavelength(corecon, gda):
+    for wavelength in gda.wavelength:
+        print("\n", wavelength)
+        get_data.get_wine_data(corecon, wavelength=wavelength)
+
+        cat_var_assertions(corecon, wavelength)
+
+
+def test_varietal(corecon, gda):
+    for varietal in gda.varietal:
+        print("\n", varietal)
+        get_data.get_wine_data(corecon, varietal=varietal)
+
+        cat_var_assertions(corecon, keyword=varietal)
+
+
+def test_wine(corecon, gda):
+    for wine in gda.wine:
+        print("\n", wine)
+        get_data.get_wine_data(corecon, wine=wine)
+        cat_var_assertions(corecon, keyword=wine)
+
+
+def test_mins(corecon, gda):
+    for mins in gda.mins:
+        print("\n", mins)
+        get_data.get_wine_data(corecon, mins=mins)
+        wd_mins = corecon.sql(
+            "SELECT min(mins), max(mins), count(mins) from wine_data"
+        ).df()
+        print(mins)
+
+        # test if minimum value is within 10% of stated mins. this happens because
+        # measurement intervals can be diff for diff runs
+        assert (mins[0] - wd_mins.at[0, "min(mins)"]) / mins[0] <= 0.1
+
+        # test if maximum value is within 10% of stated max mins. as above
+        assert (mins[1] - wd_mins.at[0, "max(mins)"]) / mins[1] <= 0.1
