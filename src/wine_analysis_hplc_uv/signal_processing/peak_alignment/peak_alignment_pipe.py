6adcf09c3dcbe1db7ca56f4278fb473646a1c8b2
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-08-22 15:00:47 +1000

    remove redundant functions
    Former-commit-id: 0927468924fb4ff4b88023af127afb0ed58e56c5
    Former-commit-id: 7d0c83a8125f0879b9402ff664c4f7254e5e6343

--- src/wine_analysis_hplc_uv/signal_processing/peak_alignment/peak_alignment_pipe.py
+++ src/wine_analysis_hplc_uv/signal_processing/peak_alignment/peak_alignment_pipe.py
@@ -52,84 +52,6 @@ from wine_analysis_hplc_uv.scripts.core_scripts import (
 from wine_analysis_hplc_uv.signal_processing import signal_alignment_methods as sa
 from wine_analysis_hplc_uv.signal_processing import signal_data_treatment_methods as sdt
 
-# create your figure and get the figure object returned
-
-
-def pickle_pipe_check(
-    db_path: str,
-    wavelength: Union[str, List[str]] = None,
-    display_in_st: bool = False,
-    pickle_dir_name: str = None,
-):
-    peak_alignment_pipe_pickle_dir_name = "peak_alignment_pipe_pickle_jar"
-    # test if pickle dir exists, if not, create it.
-    if not os.path.isdir(f"{peak_alignment_pipe_pickle_dir_name}"):
-        create_pipe_pickle = input(
-            "no peak alignment pipe pickle dir detected, create now? (y/n): "
-        )
-        if create_pipe_pickle == "y":
-            os.mkdir(f"{peak_alignment_pipe_pickle_dir_name}")
-            print(f"pickle jar created at {peak_alignment_pipe_pickle_dir_name}")
-        elif create_pipe_pickle == "n":
-            peak_alignment_pipe_pickle_dir_name = None
-            df = peak_alignment_pipe(
-                db_path,
-                wavelength,
-                pickle_path_prefix=peak_alignment_pipe_pickle_dir_name,
-            )
-        else:
-            print("bad input, try again.")
-
-    if os.path.isdir(f"{peak_alignment_pipe_pickle_dir_name}"):
-        use_prev_pickle = input("use previous peak alignment pipe pickle? (y/n): ")
-        if use_prev_pickle == "y":
-            df = peak_alignment_pipe(
-                db_path, wavelength, peak_alignment_pipe_pickle_dir_name
-            )
-        elif use_prev_pickle == "n":
-            shutil.rmtree(peak_alignment_pipe_pickle_dir_name)
-            os.mkdir(peak_alignment_pipe_pickle_dir_name)
-            df = peak_alignment_pipe(
-                db_path,
-                wavelength,
-                pickle_path_prefix=peak_alignment_pipe_pickle_dir_name,
-            )
-        else:
-            print("bad input try again")
-
-
-def rw_pipe_pickle(series: pd.Series, pickle_dir_path: str = None):
-    """
-    read and write a series of dataframes to store parts of the peak alignment pipe.
-    """
-    if pickle_dir_path == None:
-        return
-
-    filepath = os.path.join(pickle_dir_path, f"{series.name}.pk")
-
-    if not os.path.isfile(filepath):
-        print(f"{filepath} does not exist, creating pickle now")
-
-        with open(filepath, "wb") as f:
-            pickle.dump(df, f)
-    else:
-        print(f"reading {filepath}")
-        with open(filepath, "rb") as f:
-            df = pickle.load(f)
-    return df
-
-
-def get_unaligned_data(
-    db_path: str,
-    pickle_path_prefix: "str" = None,
-    wavelength: Union[str, List[str]] = None,
-):
-    con = db.connect(db_path)
-    raw_signal_col_name = f"raw {wavelength}"
-    df = get_library(con, wavelength, raw_signal_col_name)
-    return df
-
-
 import pandera as pa
 from pandera.typing import DataFrame, Series
 
@@ -186,48 +108,6 @@ def peak_alignment_pipe(df: DataFrame[PeakAlignmentPipeSchema]):
     return df
 
 
-def get_library(
-    con: db.DuckDBPyConnection, wavelength: str, signal_col_name: str
-) -> pd.DataFrame:
-    # get the selected runs to build the library
-    df = fetch_sample_dataframes_with_spectra(con)
-    df = df.set_index("name_ct", drop=True)
-
-    # select the 254nm wavelength column across the library
-
-    df[signal_col_name] = dt.subset_spectra(df["spectra"], wavelength)
-
-    # 2023-08-21 14:53:06 at this point its a long form df with [name_ct, mins, value]
-    df = df.drop("spectra", axis=1)
-
-    return df
-
-
-def fetch_sample_dataframes_with_spectra(con: db.DuckDBPyConnection) -> None:
-    with con:
-        query = """
-        SELECT
-            acq_date,
-            join_samplecode,
-            vintage_ct,
-            name_ct,
-            varietal,
-            hash_key
-        FROM super_table
-        WHERE
-            super_table.hash_key LIKE '%ed669e74%'
-            OR super_table.hash_key LIKE '%513fd979%'
-            OR super_table.hash_key LIKE '%112f4287%'
-            OR super_table.hash_key LIKE '%bf648dfb%'
-            --OR super_table.hash_key LIKE '%9b7abe4e%'
-        """
-
-        df = con.sql(query).df()
-        df = db_methods.get_spectra(df, con)
-
-    return df
-
-
 def sample_name_signal_df_builder(df_series: pd.Series):
     # extract the sample names as column names, y_axis column as column values.
     sample_name_signal_df = pd.DataFrame(columns=df_series.index)
@@ -270,21 +150,3 @@ def peak_alignment_st_output(series: pd.Series) -> None:
     st.plotly_chart(fig)
 
     return series
-
-
-def get_mock_df():
-    df = pd.DataFrame(dict(mins=[1, 2, 3, 4, 5], signal=[0, 10, 100, 10, 0]))
-    return dict(df1=df)
-
-
-def main():
-    pickle_filepath = "alignment_df_pickle.pk1"
-    db_path = definitions.DB_PATH
-    wavelength = "254"
-    display_in_st = True
-
-    pickle_pipe_check(db_path, wavelength, pickle_filepath)
-
-
-if __name__ == "__main__":
-    main()
