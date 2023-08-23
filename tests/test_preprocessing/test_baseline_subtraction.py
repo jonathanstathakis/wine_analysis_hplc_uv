b77923cbf70edfad075dd0818ba1d74e6a2ba146
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-08-23 11:08:44 +1000

    add unique samplecode test to check_dataframe_props. add 'bad_mock_df' with a duplicate samplecode,
    Former-commit-id: 77c76aa17c89a7990012c9ccbe0fb69ef14f6f82
    Former-commit-id: a5774fdea9e9e287980cae1d2af85736b17c00dd

--- tests/test_preprocessing/test_baseline_subtraction.py
+++ tests/test_preprocessing/test_baseline_subtraction.py
@@ -37,7 +37,7 @@ rules:
 
 
 @pytest.fixture
-def mock_df():
+def good_mock_df():
     return (
         pd.DataFrame(
             {
@@ -58,11 +58,44 @@ def mock_df():
 
 
 @pytest.fixture
+def bad_mock_df():
+    return (
+        pd.DataFrame(
+            {
+                ("100", "2000 wine", "mins"): [0, 1, 2, 3, 4, 5],
+                ("100", "2000 wine", "value"): [5, 10, 50, 100, 50, 10],
+                ("200", "1998 wine", "mins"): [0, 1, 2, 3, 4, 5],
+                ("200", "1998 wine", "value"): [0, 100, 0, 25, 100, 10],
+                ("201", "nv wine", "mins"): [0, 1, 2, 3, 4, 5],
+                ("201", "nv wine", "value"): [0, 100, 0, 25, 100, 10],
+                ("201", "wine", "mins"): [0, 1, 2, 3, 4, 5],
+                ("201", "wine", "value"): [0, 100, 0, 25, 100, 10],
+            }
+        )
+        .rename_axis(["samplecode", "wine", "vars"], axis=1)
+        .rename_axis("i", axis=0)
+        # .pipe(lambda df: df if print(df) is None else df)
+    )
+
+
+def test_mock_df(mock_df):
+    index_frame = mock_df.columns.to_frame().reset_index(drop=True)
+
+    print(mock_df.iloc[:, :4])
+    with pd.option_context("display.multi_sparse", False):
+        print(mock_df.iloc[:, :4])
+
+    # print(index_frame)
+    assert False
+
+
+@pytest.fixture
 def pwd(corecon):
     get_data.get_wine_data(
         con=corecon,
-        detection=("cuprac",),
-        varietal=("shiraz",),
+        # detection=("cuprac",),
+        # varietal=("shiraz",),
+        samplecode=("116",),
         wavelength=(450,),
         mins=(0, 30),
     )
@@ -72,31 +105,23 @@ def pwd(corecon):
     return df
 
 
-def test_multiindex_level_0(pwd) -> None:
+def check_dataframe_props(df: pd.DataFrame) -> None:
     """
-    test whether name is 'samplecode'. as per level_1 test, lack of standardisation
-    means its hard to right a robust test.
+    2023-08-23 09:56:10
+
+    Describes a dataframe of the shape:
+
+    samplecode 100       100       200       200
+    wine       2000 wine 2000 wine 1998 wine 1998 wine
+    vars       mins      value     mins      value
+    i
+    0           0           5       0           0
+
+    I.e. a 3 level multiindex of ('samplecode','wine','vars') and vars consists of
+    ['mins','value'] for each sample. Each samplecode should be unique, wine labels
+    are not and are there for human-readability.
+
     """
-
-
-def test_multiindex_level_1(pwd) -> None:
-    """
-    test whether name is 'wine'. Hard to conduct any further testing at this point due
-    to lack of standardisation of wine name patterns. can develop later if nes.
-    """
-
-
-def test_multiindex_level_2(pwd) -> None:
-    """
-    Test level 2 multiindex by check members and repeated pattern of ['mins','value']
-    """
-    names = pwd.columns.names
-
-    # the pipe assumes that the vars index level follows pattern ['mins','value'], and
-    # only contains those values (2023-08-22 11:56:49 this may change later)
-
-
-def check_dataframe_props(df: pd.DataFrame):
     assert df.columns.names[0] == "samplecode"
     assert df.columns.names[1] == "wine"
     assert df.columns.names[2] == "vars"
@@ -109,6 +134,25 @@ def check_dataframe_props(df: pd.DataFrame):
     assert (
         pattern * (len(vars_values) // pat_len) == vars_values
     ), "incorrect pattern sequence"
+
+    # because get_level_values returns 1 label value per sub column, end up with
+    # lots of duplicates for higher levels. `DataFrameGroupBy.size()` will be expected
+    # to return all groups of the same size. Any groups larger than the average will
+    # indicate duplicates.
+    mask = df.columns.get_level_values(0).duplicated()
+
+    samplecode = df.columns.get_level_values(0)
+    mode = samplecode.value_counts().mode()[0]
+    outlier_mask = samplecode.value_counts() > 2
+    duplicates = outlier_mask[outlier_mask == True].dropna().index.values
+
+    assert len(duplicates) == 0, duplicates
+
+    return None
+
+
+def test_check_dataframe_props(mock_df: pd.DataFrame) -> None:
+    check_dataframe_props(mock_df)
 
 
 def test_baseline(pwd):
@@ -136,7 +180,7 @@ def test_baseline(pwd):
         .reindex(["mins", "value", "baseline", "value_bcorr"], level=2, axis=1)
     )
     print(out)
-    out.loc[:, pd.IndexSlice["154", :, ["value", "baseline"]]].plot()
-    plt.show()
+    # out.loc[:, pd.IndexSlice["154", :, ["value", "baseline"]]].plot()
+    # plt.show()
 
     assert False
