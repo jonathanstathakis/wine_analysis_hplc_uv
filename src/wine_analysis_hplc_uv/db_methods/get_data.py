2c630de648b747020c4a4018c55d6ea5100a1632
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-08-23 11:08:57 +1000

    shorten lines to match spec
    Former-commit-id: e760cb47eaf8bb9df5b8e91e8fa1b85cece9f3a0
    Former-commit-id: b22fcf319c511ff1ba161141db5a28f89718405e

--- src/wine_analysis_hplc_uv/db_methods/get_data.py
+++ src/wine_analysis_hplc_uv/db_methods/get_data.py
@@ -19,9 +19,8 @@ def get_wine_data(
     mins: tuple = (None, None),
 ):
     """
-    Select all CUPRAC wines, getting their metadata as well by first joining the
-    sample tracker and cellar tracker tables, then chemstation metadata then finally
-    chemstation data table.
+    Join the sample tracker and cellar tracker tables, then chemstation metadata then
+    finally chemstation data table.
 
     ## Tables in DB
 
@@ -36,7 +35,8 @@ def get_wine_data(
 
     ## sample_tracker columns
 
-    Index(['detection', 'sampler', 'samplecode', 'vintage', 'name', 'open_date','sampled_date', 'added_to_cellartracker', 'notes', 'size' 'ct_wine_name'],
+    Index(['detection', 'sampler', 'samplecode', 'vintage', 'name', 'open_date',
+    'sampled_date', 'added_to_cellartracker', 'notes', 'size' 'ct_wine_name'],
 
     ## c_sample_tracker shape
 
@@ -44,7 +44,8 @@ def get_wine_data(
 
     ## cellar_tracker_columns
 
-    Index(['size', 'vintage', 'name', 'locale', 'country', 'region', 'subregion','appellation', 'producer', 'type', 'color', 'category', 'varietal',
+    Index(['size', 'vintage', 'name', 'locale', 'country', 'region', 'subregion',
+    'appellation', 'producer', 'type', 'color', 'category', 'varietal',
        'wine'],
 
     ## c_cellar_tracker shape
@@ -99,12 +100,19 @@ def get_wine_data(
             )
             LEFT JOIN chromatogram_spectra cs ON (chm.id = cs.id)
         WHERE
-          ((SELECT UNNEST($detection)) IS NULL OR st.detection IN (SELECT * FROM UNNEST($detection)))
-            AND ((SELECT UNNEST($samplecode)) IS NULL OR st.samplecode IN (SELECT * FROM UNNEST($samplecode))) AND st.samplecode NOT IN ('72','98')
-            AND ((SELECT UNNEST($color)) IS NULL OR ct.color IN (SELECT * FROM UNNEST($color)))
-            AND ((SELECT UNNEST($varietal)) IS NULL OR ct.varietal IN (SELECT * FROM UNNEST($varietal)))
-            AND ((SELECT UNNEST($wine)) IS NULL OR ct.wine IN (SELECT * FROM UNNEST($wine)))
-            AND ((SELECT UNNEST($wavelength)) IS NULL OR cs.wavelength IN (SELECT * FROM UNNEST($wavelength)))
+          ((SELECT UNNEST($detection)) IS NULL
+            OR st.detection IN (SELECT * FROM UNNEST($detection)))
+            AND ((SELECT UNNEST($samplecode))
+                IS NULL OR st.samplecode IN (SELECT * FROM UNNEST($samplecode)))
+                AND st.samplecode NOT IN ('72','98')
+            AND ((SELECT UNNEST($color)) IS NULL
+                OR ct.color IN (SELECT * FROM UNNEST($color)))
+            AND ((SELECT UNNEST($varietal)) IS NULL
+                OR ct.varietal IN (SELECT * FROM UNNEST($varietal)))
+            AND ((SELECT UNNEST($wine)) IS NULL
+                OR ct.wine IN (SELECT * FROM UNNEST($wine)))
+            AND ((SELECT UNNEST($wavelength)) IS NULL
+                OR cs.wavelength IN (SELECT * FROM UNNEST($wavelength)))
             AND ($min_start IS NULL OR cs.mins >= $min_start)
             AND ($min_end IS NULL OR cs.mins <= $min_end)
         """,
