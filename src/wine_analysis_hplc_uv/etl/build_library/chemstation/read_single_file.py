8822e36ac1e50c9b271c12c78ebdccfe618c3cb3
jstathakis <62565951+jonathanstathakis@users.noreply.github.com>
2024-09-17 23:31:16 +1000

    Update read_single_file.py

--- src/wine_analysis_hplc_uv/etl/build_library/chemstation/read_single_file.py
+++ src/wine_analysis_hplc_uv/etl/build_library/chemstation/read_single_file.py
@@ -7,6 +7,20 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+def uv_data_to_df(uv_file: rb.DataFile) -> pd.DataFrame:
+    """
+    requires a parsed `rb.DataFile` as input, outputs a df.
+    """
+    spectrum = np.concatenate(
+            (uv_file.xlabels.reshape(-1, 1), uv_file.data), axis=1
+        )
+
+    column_names = ["mins"] + list(uv_file.ylabels)
+    column_names = [str(name) for name in column_names]
+
+    an_index = np.arange(0, spectrum.shape[0])
+
+    return pd.DataFrame(data=spectrum, columns=column_names, index=an_index)
 
 def read_single_file(
     path: str,
@@ -18,31 +32,7 @@ def read_single_file(
     metadata_dict contains 'path', 'sequence_name', 'hash_key' items. uv_data_dict contains 'data' and 'hash_key' items.
     """
 
-    def uv_data_to_df(uv_file: rb.DataFile) -> pd.DataFrame:
-        """
-        requires a parsed `rb.DataFile` as input, outputs a df.
-        """
-        spectrum = np.concatenate(
-            (uv_file.xlabels.reshape(-1, 1), uv_file.data), axis=1
-        )
-
-        column_names = ["mins"] + list(uv_file.ylabels)
-        column_names = [str(name) for name in column_names]
-
-        an_index = np.arange(0, spectrum.shape[0])
-
-        try:
-            df = pd.DataFrame(data=spectrum, columns=column_names, index=an_index)
-
-            return df
-        except Exception as e:
-            logger.error(e)
-            logger.error(uv_file.metadata.get("notebook"), uv_file.metadata.get("date"))
-            return None
-
     uv_name = "DAD1.UV"
-
-    assert os.path.isfile(path=os.path.join(path, uv_name))
 
     metadata_dict = dict(
         path=path,
@@ -52,8 +42,7 @@ def read_single_file(
         data=pd.DataFrame(),
         id="",
     )
-
-    # try:
+    
     datadir = rb.read(path=path)
     uv_file = datadir.get_file(filename=uv_name)
 
@@ -65,14 +54,7 @@ def read_single_file(
     uv_data_dict["data"] = uv_data_to_df(uv_file=uv_file)
     uv_data_dict["id"] = metadata_dict["id"]
 
-    # except Exception as e:
-    #     logger.error(f"{metadata_dict['path']} encountered an error: {e}")
-
-    returndict: Dict[
-        str, Union[Dict[str, str], Dict[str, Union[str, pd.DataFrame]]]
-    ] = {
+    return {
         "metadata": metadata_dict,
         "data": uv_data_dict,
     }
-
-    return returndict
